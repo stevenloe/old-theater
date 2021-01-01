3a3eb5876415a74b5252088cf235cd700c870ca9
Steven Loe <stevenjloe@protonmail.com>
2021-01-01 15:40:45 -0500

    fix border

--- website/src/components/MemberCrawl.vue
+++ website/src/components/MemberCrawl.vue
@@ -1,6 +1,6 @@
 <template>
  
-  <div class=" p-8 border border-blue-900">
+  <div class="p-8 border-t-2 border-gray-500">
     <h2 class="mb-4 text-2xl font-semibold">Thanks to Our 2020 Members!</h2>
     <div class="flex justify-between">
       <p class="text-2xl font-serif text-gray-600">Kathy Enzerink</p>
