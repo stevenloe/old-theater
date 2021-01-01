49a9286d147e674d112e53230061855daa33d365
Steven Loe <stevenjloe@protonmail.com>
2020-12-31 21:49:30 -0500

    Add CTA buttons to show detail cards. Improve layout - especially on medium screens

--- website/src/components/ShowCard.vue
+++ website/src/components/ShowCard.vue
@@ -1,22 +1,32 @@
 <template>
-  <div class="m-8 md:flex">
-    <div class="relative pb-5/6">
+  <!-- image card-->
+  <div class="m-8 md:mr-4 md:flex border border-red-900">
+    <div class="relative pb-5/6 md:w-2/3 border border-white">
       <img class="h-full w-full object-cover rounded-lg shadow-md" :src="item.imageUrl" :alt="item.imageAlt">
     </div>
-    <div class="relative px-4 -mt-4 md:-ml-6 md:mt-6 md:mb-6 md:flex">
-      <div class="bg-white p-6 rounded-lg shadow-lg">
-        <div class="flex items-baseline">
-          <span class="inline-block bg-red-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide">{{item.category}}</span>
+
+    <!-- details card-->
+    <div class="relative  px-4 -mt-4 md:-ml-4 md:mt-6 md:mb-6 md:flex md:w-1/3 border md:p-0 border-green-500">
+      <div class="bg-gray-200 p-6 rounded-lg shadow-lg w-full">
+        <div class="">
+          <div class="inline-block  bg-red-200 text-teal-900 text-xs px-2 rounded-full uppercase font-semibold tracking-wide">{{item.category}}</div>
+
+          <h4 class="ml-2 mt-1 font-semibold text-lg leading-tight truncate">{{ item.title }}</h4>
           <div class="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide">
-            {{ item.date }}  &bull; {{ item.time}}  &bull; At {{ item.location}} 
+            {{ item.date }} &bull; {{ item.time}} &bull; ${{ item.formattedPrice }}
           </div>
-          
+          <!-- <div class="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide">At {{ item.location}}</div> -->
         </div>
-        <h4 class="mt-1 font-semibold text-lg leading-tight truncate">{{ item.title }}</h4>
-        <div class="mt-1">
-          ${{ item.formattedPrice }}
-          <span class="text-gray-600 text-sm"></span>
+
+        <p class="ml-2 truncate md:break-normal border border-blue-400"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda aliquid minima ipsum sint perspiciatis expedita autAssumenda aliquid minima ipsum sint perspiciatis expedita aut ullam sed ut inventore vitae cupiditate labore eos doloribus, optio cum tempore facere officia.</p>
+
+        <!-- <div v-bind:style="styleObject"></div> -->
+
+        <div class="flex w-full mt-2 md:inline-block border border-green-400 ">
+          <button class="w-1/2 bg-yellow-200 md:w-full text-gray-800 font-bold py-1 px-4 border border-gray-800 rounded-lg shadow hover:bg-yellow-300 hover:text-gray-900">Buy Tickets</button>
+          <button class="w-1/2 ml-3 md:ml-0 md:mt-3 md:w-full bg-white  text-gray-700 font-semibold py-1 px-4 border border-gray-600 rounded-lg shadow hover:bg-gray-100 hover:text-gray-900">Learn More</button>
         </div>
+
       </div>
     </div>
   </div>
@@ -24,6 +34,15 @@
 
 <script>
 export default {
-  props: ['item'],
-}
+  props: ["item"],
+  computed: {
+    // classObject: function () {
+    //   return {
+    //     // active: this.isActive && !this.error,
+    //     // 'text-danger': this.error && this.error.type === 'fatal'
+    //     "foo"
+    //   }
+    // }
+  },
+};
 </script>
--- website/src/pages/Index.vue
+++ website/src/pages/Index.vue
@@ -20,68 +20,68 @@ export default {
     data() {
     return {
       items: [
-        {
+        { 
           title: "Scrooged",
           date: "December 21, 2020",
           time: "7:30 PM",
           category: "Friday Flick",
           location: "The Red Rooster",
           description: "A book on philosphy about the origins of man",
-          imageUrl: "/img/poster/scrooged.png",
+          imageUrl: "/img_new/processed/scrooged_result.jpg",
           imageAlt: "image alt text",
           formattedPrice: "Donation",
         },
-        {
+        { 
           title: "Goldfinger",
           date: "January 11, 2020",
           time: "7:00 PM",
           category: "Friday Flick",
           location: "The Red Rooster",
           description: "The true search for one's treasure",
-          imageUrl: "/img/poster/goldfinger.png",
+          imageUrl: "/img_new/processed/goldfinger_result.jpg",
           imageAlt: "Some Alt Text",
           formattedPrice: "Donation",
         },
-        {
+        { 
           title: "Nunsense",
           date: "February 11, 2020",
           time: "7:00 PM",
           category: "Theater",
           location: "The Old Theater",
           description: "Sci-Fi Futuristic book about mind reading",
-          imageUrl: "/img/poster/nunsense.png",
+          imageUrl: "/img_new/processed/nunsense-big_result.jpg",
           imageAlt: "Some Alt Text",
           formattedPrice: "21.99",
         },
-        {
+        { 
           title: "Barbra Lica",
           date: "February 1, 2020",
           time: "7:00 PM",
           category: "Live Music",
           location: "The Old Theater",
           description: "Motivational book on wealth building",
-          imageUrl: "/img/poster/barbra-lica.png",
+          imageUrl: "/img_new/processed/barbra-lica_result.jpg",
           imageAlt: "Some Alt Text",
           formattedPrice: "11.99",
         },
-        {
+        { 
           title: "Education",
           date: "February 11, 2020",
           time: "7:00 PM",
           category: "Education",
           description: "Conspiracy theories about secrets of the holy grail",
-          imageUrl: "/img/poster/education.png",
+          imageUrl: "/img_new/processed/education_result.jpg",
           imageAlt: "Some Alt Text",
           formattedPrice: "17.99",
         },
-        {
+        { 
           title: "Support",
           date: "February 11, 2020",
           time: "7:00 PM",
           category: "Support",
           location: "The Old Theater",
           description: "Travel back in time and experience the banking system",
-          imageUrl: "/img/poster/support.png",
+          imageUrl: "/img_new/processed/cast-edit_result.jpg",
           imageAlt: "Some Alt Text",
           formattedPrice: "16.99",
         },
