787a78c3e975589f3ec9ac8e9ed061344d29330d
Steven Loe <stevenjloe@protonmail.com>
2021-01-03 14:14:53 -0500

    add pages

---
+++ website/src/pages/Calendar.vue
@@ -0,0 +1,25 @@
+<template>
+  <ContentLayout>
+    <div class="container md:mx-auto max-w-3xl">
+
+      <div class="mb-4">
+        <h1 class="text-3xl font-semibold">Calendar</h1>
+        <img src="/temp/january-caledar.jpg" class=" w-full mr-3  " alt="">
+      </div>
+    </div>
+
+  </ContentLayout>
+</template>
+
+<script>
+import ContentLayout from "~/layouts/Content.vue";
+export default {
+  metaInfo: {
+    title: "About us",
+  },
+  components: {
+
+    ContentLayout,
+  },
+};
+</script>
