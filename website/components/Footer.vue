2dbfbe4184f4238d90f05a76e9869a3013d74dce
Steven Loe <stevenjloe@protonmail.com>
2021-01-15 17:40:56 -0500

    footer layout improvements

--- website/components/Footer.vue
+++ website/components/Footer.vue
@@ -1,31 +1,28 @@
 <template>
-  <footer class="bg-gray-300 pt-6 pb-6">
-    <div class="m-8  mt-0 text-center md:w-8/12 lg:w-7/12  xl:w-8/12 mx-auto">
-      <img class="inline-block h-20" src="~/assets/img/old-theater-logo-final.svg" alt="The Old Theater, Oriental North Carolina">
+  <footer class="bg-gray-300 p-8 ">
+    <div class="mt-0 text-center sm:w-3/4 md:w-8/12 lg:w-1/2  xl:w-5/12 mx-auto">
+      <img class="inline-block h-20 md:h-24" src="~/assets/img/old-theater-logo-final.svg" alt="The Old Theater, Oriental North Carolina">
 
-      <address class="-mt-2 mb-8 not-italic font-serif text-xl w-full xl:text-xl">
+      <address class="-mt-2 mb-8 not-italic font-serif md:text-xl w-full xl:text-xl">
         The Old Theater<br>
         609 Broad Street<br>
         Oriental, NC 28571<br>
         (252) 249 &ndash; 0477<br>
       </address>
+      <nav class="flex border-t pt-1 pb-1 mb-8 border-b border-gray-500">
+        <div class="w-1/2 text-left">
+          <a href="#" class="footer-link tracking-wider md:text-lg hover:text-black">NEWS</a>
+          <a href="#" class="footer-link tracking-wider md:text-lg hover:text-black">OUR HISTORY</a>
+          <a href="#" class="footer-link tracking-wider md:text-lg hover:text-black">CONTACT US</a>
+        </div>
+        <div class="w-1/2 text-right">
+          <a href="#" class="footer-link tracking-wider md:text-lg  hover:text-black">BOARD OF DIRECTORS</a>
+          <a href="#" class="footer-link tracking-wider  md:text-lg hover:text-black">HALL OF FAME</a>
+          <a href="#" class="footer-link tracking-wider md:text-lg  hover:text-black">VENUE</a>
+        </div>
+      </nav>
 
-    
-        <nav class="flex w-full mb-8 pt-1 pb-1  border-t border-b border-gray-500">
-          <div class="w-1/2 text-left">
-            <a href="#" class="footer-link tracking-wider md:text-lg hover:text-black">NEWS</a>
-            <a href="#" class="footer-link tracking-wider md:text-lg hover:text-black">OUR HISTORY</a>
-            <a href="#" class="footer-link tracking-wider md:text-lg hover:text-black">CONTACT US</a>
-          </div>
-          <div class="w-1/2 text-right">
-            <a href="#" class="footer-link tracking-wider md:text-lg  hover:text-black">BOARD OF DIRECTORS</a>
-            <a href="#" class="footer-link tracking-wider  md:text-lg hover:text-black">HALL OF FAME</a>
-            <a href="#" class="footer-link tracking-wider md:text-lg  hover:text-black">VENUE</a>
-          </div>
-        </nav>
-  
-
-      Website by <a class="text-gray-700 hover:text-gray-900 md:text-lg " href="https://stevenloe.com">Steven Loe</a>
+      <a class="w-full text-gray-700 hover:text-gray-900 mb-16 md:text-lg " href="https://stevenloe.com">Website by Steven Loe</a>
 
     </div>
   </footer>
