25418f31abb03170c33e3bdba2b79898691baacf
Steven Loe <stevenjloe@protonmail.com>
2021-01-18 18:02:46 -0500

    feat: (design) add layout for conent pages

---
+++ website/layouts/content.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="antialiased text-gray-900 bg-gray-200 mb-12"> 
+    <NavBar/>
+    <div>
+      <Nuxt />
+    </div>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar'
+import Footer from '@/components/Footer'
+
+export default {
+  components: {
+    NavBar,
+    Footer
+  },
+}
+</script>
+
+<style>
+/* html {
+  font-family:
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+} */
+
+
+</style>
