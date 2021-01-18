6e05bddfdb2eb47fe98a08ac4127a540ce30376b
Steven Loe <stevenjloe@protonmail.com>
2021-01-18 14:42:38 -0500

    design: (education widget) new colors contrast+

--- website/components/Education.vue
+++ website/components/Education.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="m-4 sm:m-8 md:m-8 ">
+  <div class="m-4 sm:m-8 md:m-8 bg-indigo-700 pt-1 rounded-lg shadow-lg ">
     <!-- image card-->
-    <div class="rounded-t-lg overflow-hidden shadow-lg">
+    <div class=" m-6  rounded-lg overflow-hidden shadow-xl">
       <img class="h-full w-full object-cover" :src="item.imageUrl" :alt="item.imageAlt">
     </div>
     <!-- details -->
-    <div class=" bg-blue-500 rounded-b-lg  pl-6 pr-6 pt-3 pb-4 shadow-lg">
+    <div class="rounded-b-lg  pl-6 pr-6 pt-0 pb-4 -mt-4">
       <h2 class="text-white  font-bold text-4xl text-shadow">{{ item.title }}</h2>
       <p class="text-white mb-5 block font-medium text-lg md:text-2xl lg:text-3xl">{{ item.description }}</p>
-      <nuxt-link to="/education" class="block mb-3 bg-green-600  text-center border-2 border-white text-white font-semibold text-lg py-2 px-4 shadow-md rounded-lg tracking-wide hover:bg-green-700 hover:shadow-sm uppercase sm:w-1/3">{{ item.cta }}</nuxt-link>
+      <nuxt-link to="/education" class="block mb-3 bg-yellow-500  text-center border-2 border-yellow-400 text-black font-semibold text-lg py-2 px-4 shadow-md rounded-lg tracking-wide hover:bg-blue-700 hover:shadow-sm uppercase sm:w-1/3">{{ item.cta }}</nuxt-link>
     </div>
   </div>
 </template>
