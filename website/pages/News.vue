1a295f2709e717b56637dc76f01ab983d79041ee
Steven Loe <stevenjloe@protonmail.com>
2021-01-18 18:31:38 -0500

    feat: (pages) add news page

---
+++ website/pages/News.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container md:mx-auto max-w-3xl">
+
+      <div class="mb-4">
+        <h1 class="text-3xl font-semibold">News</h1>
+        
+        <div class="text-lg font-semibold text-red-400 mb-64">
+          List of news articles goes here
+        </div>
+
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  metaInfo: {
+    title: "News",
+  },
+    layout: "content",
+};
+</script>
