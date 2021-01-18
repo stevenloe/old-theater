72b5193a2b679300e69defe3b1e556939606d166
Steven Loe <stevenjloe@protonmail.com>
2021-01-18 16:47:20 -0500

    fix: (about) restore <article> tag

--- website/pages/about/_slug.vue
+++ website/pages/about/_slug.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="">
     <NavBar class="sm:mb-8" />
-    <about>
+    <article>
       <div class="sm:rounded-lg sm:ml-8 sm:mr-8 overflow-hidden shadow-lg mb-6 max-w-6xl mx-auto">
         <img :src="require(`~/assets/img_new/processed/${about.img}`)" :alt="about.imgAlt" />
       </div>
@@ -9,7 +9,7 @@
       <div class="pl-8 pr-8 sm:max-w-lg  md:max-w-xl md:text-4xl lg:max-w-3xl  xl:max-w-4xl   sm:bg-red-200  md:bg-orange-200 lg:bg-yellow-400 mx-auto">
         <nuxt-content :document="about" class="prose max-w-none md:prose-lg lg:prose-xl lg:prose-2xl mb-8" />
       </div>
-    </about>
+    </article>
 
   </div>
 </template>
