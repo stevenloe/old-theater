eaa984e05526acf3c63367c51c49ce17f7d72666
Steven Loe <stevenjloe@protonmail.com>
2021-01-18 18:20:55 -0500

    feat: (pages) add contact page

---
+++ website/pages/Contact.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container md:mx-auto max-w-3xl">
+      <div class="mb-4">
+        <h1 class="text-3xl font-semibold">Contact</h1>
+        <div class="text-lg font-semibold text-red-500 mb-64">
+          contact form goes here
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  metaInfo: {
+    title: "Contact",
+  },
+    layout: "content",
+};
+</script>
