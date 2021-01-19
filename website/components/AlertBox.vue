37b25571cf947ee0c9ee5d4d1f3b54395bae00e0
Steven Loe <stevenjloe@protonmail.com>
2021-01-18 19:39:07 -0500

    design: (alert box)  should not grow during render

--- website/components/AlertBox.vue
+++ website/components/AlertBox.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="rounded-lg m-4 sm:m-8 md:flex md:m-8 md:mr-8 bg-yellow-100 border-2 border-yellow-500">
-    <p class="mx-4 my-2 font-semibold">In response to COVID-19, The Old Theater is presenting outdoor events only.
-      <nuxt-link to="/alert/covid-19" class="hover:bg-gray-100 hover:text-gray-900">Learn More</nuxt-link>
+  <div class="rounded-lg m-4 sm:m-8 md:m-8 md:mr-8 bg-yellow-100 border-2 border-yellow-500">
+    <p class="px-4 py-2 font-semibold">In response to COVID-19, The Old Theater is presenting outdoor events only.
+      <nuxt-link to="#" class="text-blue-900 hover:bg-gray-100 hover:text-blue-800">Learn More</nuxt-link>
     </p>
   </div>
 </template>
