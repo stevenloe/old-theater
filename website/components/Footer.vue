e4d52d73d4b557c72a31a106fbdc3ce293e66076
Steven Loe <stevenjloe@protonmail.com>
2021-01-18 21:17:56 -0500

    fix: (footer) off-site links should be <a href>

--- website/components/Footer.vue
+++ website/components/Footer.vue
@@ -28,7 +28,7 @@
         </div>
       </nav>
 
-      <nuxt-link to="https://stevenloe.com" class="w-full text-gray-700 hover:text-gray-900 mb-16 text-sm sm:text-base  " href="https://stevenloe.com">Website by Steven Loe</nuxt-link>
+      <a href="https://stevenloe.com" class="w-full text-gray-700 hover:text-gray-900 mb-16 text-sm sm:text-base ">Website by Steven Loe</a>
 
     </div>
 
