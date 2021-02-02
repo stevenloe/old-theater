1d39fe23332004046ce28ae54125a82d7d6032a5
Steven Loe <stevenjloe@protonmail.com>
2021-02-01 22:08:45 -0500

    fix: (alertbox) fix resize on menu flyout

--- website/components/AlertBox.vue
+++ website/components/AlertBox.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="rounded-lg m-4 sm:m-8 md:m-8 md:mr-8 bg-yellow-100 border-2 border-yellow-500">
+  <div class="rounded-lg m-4 sm:m-8 md:m-8 md:mr-8 bg-yellow-300">
     <p class="px-4 py-2 font-semibold">In response to COVID-19, The Old Theater is presenting outdoor events only.
       <nuxt-link to="#" class="text-blue-900 hover:bg-gray-100 hover:text-blue-800">Learn More</nuxt-link>
     </p>
