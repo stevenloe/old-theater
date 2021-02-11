168358fd351861e6beaa991754ad9b817ed5064a
Steven Loe <stevenjloe@protonmail.com>
2021-02-11 14:59:37 -0500

    fix: (submenu) improve appearance - caret

--- website/components/SubMenu.vue
+++ website/components/SubMenu.vue
@@ -2,17 +2,28 @@
   <div
     @mouseenter="isMouseOver = 1"
     @mouseleave="isMouseOver = 0"
-    class="block relative text-2xl md:py-1 md:px-2 md:text-lg rounded md:hover:bg-gray-200 mt-2 z-30
-    ">
-    <span class="text-gray-600 md:text-gray-900">
+    class="block relative text-2xl md:py-1 md:px-2 md:text-lg rounded md:hover:bg-gray-200 mt-2 z-30"
+  >
+    <span class="inline-block text-gray-600 md:text-gray-900">
       {{ menuData.title }}
     </span>
 
-    <span class="text-sm text-white md:text-gray-500 tracking-tighter">&#9660;</span>
+    <span class="inline-block">
+      <svg
+        class="fill-current text-white md:text-gray-800 h-4 w-4"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+      >
+        <path
+          d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+        />
+      </svg>
+    </span>
 
     <div
       v-show="isOpen"
-      class="md:w-48 mt-1 ml-4 block p-3 bg-white z-50 md:absolute md:shadow-xl md:-ml-2">
+      class="md:w-48 mt-1 ml-4 block p-3 bg-white z-50 md:absolute md:shadow-xl md:-ml-2"
+    >
       <nuxt-link
         v-for="item in menuData.items"
         v-bind:item="item"
@@ -29,11 +40,11 @@
 export default {
   props: {
     menuData: Object,
-    isMobile: Boolean
+    isMobile: Boolean,
   },
   data() {
     return {
-      isMouseOver: false
+      isMouseOver: false,
     };
   },
   computed: {
@@ -42,7 +53,7 @@ export default {
         return true;
       }
       return this.isMouseOver === 1 ? true : false;
-    }
-  }
+    },
+  },
 };
 </script>
