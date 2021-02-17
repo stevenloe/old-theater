8e8ef964976f1e0a1b7ce54d27562ec4ed44f906
Steven Loe <stevenjloe@protonmail.com>
2021-02-17 16:17:33 -0500

    design: (home) content area shouldn't get too wide

--- website/pages/index.vue
+++ website/pages/index.vue
@@ -4,15 +4,13 @@
       <NavBar />
     </client-only>
 
-    <AlertBox />
-
-    <div class="articles">
+    <div class="container mx-auto">
+      <AlertBox />
       <div
         class="show-card m-4 sm:m-8 md:flex md:m-8 md:mr-8 rounded-lg p-0"
         v-for="article of articles"
         :key="article"
       >
-        <!-- <pre>{{ article }}</pre> -->
         <!-- image card -->
         <div
           class="relative md:w-2/3 md:rounded-r-none rounded-lg overflow-hidden"
@@ -29,7 +27,7 @@
         <div
           class="relative -mt-1 bg-gray-200 rounded-b-lg md:mt-0 md:rounded-b-none md:rounded-r-lg md:w-1/3 md:p-0"
         >
-          <div class="p-6 pt-2 w-full">
+          <div class="p-6 pt-2 pb-0 w-full">
             <h3 class="font-bold text-4xl">{{ article.title }}</h3>
             <div class="text-xs uppercase font-semibold tracking-wide">
               {{ article.eventTime | formatDate }} •
@@ -42,30 +40,26 @@
             </p>
           </div>
 
-          <div class="flex w-full  md:inline-block pl-6 pr-6 pb-4">
+          <div class="flex w-full md:inline-block pl-6 pr-6 pb-4">
             <nuxt-link
               to="#"
-              class="w-1/2 bg-blue-600 md:w-full md:block text-white uppercase shadow-md rounded-lg tracking-wide font-bold py-2 px-4 text-lg text-center hover:bg-blue-700 mb-3 mr-2"
+              class="w-1/2 bg-blue-600 md:w-full md:block text-white uppercase shadow-lg rounded-lg tracking-wide font-bold py-2 px-4 text-lg text-center hover:bg-blue-700 mb-3 mr-2"
               >Buy Tickets</nuxt-link
             >
             <nuxt-link
               :to="article.slug"
-              class="w-1/2 bg-gray-600 md:w-full md:block text-white uppercase shadow-md rounded-lg tracking-wide font-bold py-2 px-4 text-lg text-center hover:bg-gray-700 mb-3"
+              class="w-1/2 bg-gray-600 md:w-full md:block text-white uppercase shadow-lg rounded-lg tracking-wide font-bold py-2 px-4 text-lg text-center hover:bg-gray-700 mb-3"
               >Learn More</nuxt-link
             >
           </div>
         </div>
       </div>
-    </div>
-
-    <NewsWidget />
-    <Education />
-    <Membership />
-    <Sponsorship />
-
-    <client-only>
+      <NewsWidget />
+      <Education />
+      <Membership />
+      <Sponsorship />
       <MemberCrawl />
-    </client-only>
+    </div>
   </div>
 </template>
 
@@ -95,29 +89,6 @@ export default {
       ])
       .sortBy("eventTime", "asc")
       .fetch();
-
-    // const n = new Date();
-    // const now = n.getTime();
-
-    // articles.forEach(element => {
-    //   const ed = new Date(element.eventDate)
-    //   const eventDate = ed.getTime();
-    //   console.log("now", now, "eventDate", eventDate);
-    //   console.log("event in future:", ed > n);
-    //   console.log("element", element)
-    // });
-
-    articles.forEach((element) => {
-      // console.log("elem", element);
-    });
-
-    // filter out past events
-    // const now = new Date()
-    // articles = articles.filter(article =>  new Date(article.eventDate) > now)
-
-    return {
-      articles,
-    };
   },
   layout: "home",
   components: {
@@ -127,10 +98,6 @@ export default {
     MemberCrawl,
     Sponsorship,
     NewsWidget,
-  },
-    created: function () {
-    // `this` points to the vm instance
-    console.log("------- CREATED INDEX ----------");
   },
 };
 </script>
