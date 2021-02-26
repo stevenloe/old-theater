8f2be8038f618a4b8cc2900f2d766e63f40af6c6
Steven Loe <stevenjloe@protonmail.com>
2021-02-26 18:37:18 -0500

    design: (about) new content page  design

--- website/pages/about/_slug.vue
+++ website/pages/about/_slug.vue
@@ -1,35 +1,43 @@
 <template>
   <div>
-     <NavBar />
-
-    <article class="pt-4 sm:pt-8 md:pt-16">
-      <div v-if="about.img" class="sm:rounded-lg sm:ml-8 sm:mr-8 overflow-hidden shadow-lg mb-6 max-w-6xl mx-auto">
-        <img :src="require(`~/assets/img/about/${about.img}`)" :alt="about.imgAlt" />
+    <!-- <header class="text-white mx-auto text-xs">
+      <div class="p-5 bg-indigo-500 md:inline-block">
+        <p>
+          <span class="uppercase">Published:</span>
+          <span>{{ about.published }}</span>
+        </p>
+        <div class="flex">
+          <span class="mr-1 uppercase">Filed under:</span>
+          <ul class="flex">
+            <li v-for="tag in about.tags" :key="tag" class="mr-2">
+              <nuxt-link
+                :to="{ name: 'tags-tag', params: { tag: tag.toLowerCase() } }"
+                class="hover:underline"
+              >
+                {{ tag }}
+              </nuxt-link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="pl-8 pr-8 pt-8 sm:pt-0 sm:max-w-lg  md:max-w-xl md:text-4xl lg:max-w-3xl  xl:max-w-4xl mx-auto">
-        <nuxt-content :document="about" class="prose max-w-none md:prose-lg lg:prose-xl lg:prose-2xl mb-8" />
-      </div>
-    </article>
+    </header> -->
+    <div class="prose prose-lg text-gray-700 bg-white p-5 pt-1 rounded shadow mx-auto">
+      <nuxt-content :document="about" />
+    </div>
+    <!-- <footer class="py-5 mt-5">
+      <prev-next :prev="prev" :next="next" />
+    </footer> -->
   </div>
 </template>
 
-
 <script>
-import NavBar from "@/components/NavBar";
+
 export default {
   async asyncData({ $content, params }) {
     const about = await $content("abouts", params.slug).fetch();
     return { about };
   },
-  components: {
-    NavBar,
-  },
+  layout:'NewLayout'
+
 };
 </script>
-
-<style >
-p {
-  margin-bottom: 2em !important;
-}
-</style>
-
