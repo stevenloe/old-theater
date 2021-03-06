2d9b9f2751541e128857f104f30ad7e1f82bc8f6
Steven Loe <stevenjloe@protonmail.com>
2021-03-06 03:59:34 -0500

    feat: add ticket vendor

--- website/pages/index.vue
+++ website/pages/index.vue
@@ -2,32 +2,51 @@
   <div class="mx-auto">
     <div class="container mx-auto">
       <AlertBox />
-      <div class="show-card m-4 sm:m-8 md:flex md:m-8 md:mr-8 rounded p-0 shadow-lg" v-for="show of shows"
-        :key="show.title">
+      <div
+        class="p-0 m-4 rounded shadow-lg show-card sm:m-8 md:flex md:m-8 md:mr-8"
+        v-for="show of shows"
+        :key="show.title"
+      >
+      
         <!-- image card -->
-        <div class="relative md:w-7/12 md:rounded-r-none rounded overflow-hidden text">
+        <div
+          class="relative overflow-hidden rounded md:w-7/12 md:rounded-r-none text"
+        >
           <nuxt-link :to="show.slug">
-            <img :src="require(`~/assets/img_new/processed/${show.img}`)" :alt="show.imgAlt" />
+            <img
+              :src="require(`~/assets/img_new/processed/${show.img}`)"
+              :alt="show.imgAlt"
+            />
           </nuxt-link>
         </div>
 
         <!-- details card -->
-        <div class="relative -mt-1 bg-white rounded-b-lg md:mt-0 md:rounded-b-none md:rounded-r-lg md:w-5/12 md:p-0 ">
-          <div class="p-6 pt-2 pb-0 w-full">
-            <h3 class="font-bold text-4xl leading-none mt-3 mb-2">{{ show.title }}</h3>
-            <div class="text-sm uppercase font-semibold tracking-wide mb-4">
+        <div
+          class="relative -mt-1 bg-white rounded-b-lg md:mt-0 md:rounded-b-none md:rounded-r-lg md:w-5/12 md:p-0"
+        >
+          <div class="w-full p-6 pt-2 pb-0">
+            <h3 class="mt-3 mb-2 text-4xl font-bold leading-none">
+              {{ show.title }}
+            </h3>
+            <div class="mb-4 text-sm font-semibold tracking-wide uppercase">
               {{ show.eventTime | formatDate }} •
               {{ show.eventTime | formatTime }} • ${{ show.formattedPrice }}
             </div>
           </div>
 
-          <div class="flex w-full md:inline-block pl-6 pr-6 pb-4">
-            <nuxt-link to="#"
-              class="w-1/2 bg-blue-600 md:w-full md:block text-white uppercase shadow-lg rounded-lg tracking-wide font-bold py-2 px-4  text-center hover:bg-blue-700 mb-3 mr-2">
-              Buy Tickets</nuxt-link>
-            <nuxt-link :to="show.slug"
-              class="w-1/2 bg-gray-600 md:w-full md:block text-white uppercase shadow-lg rounded-lg tracking-wide font-bold py-2 px-4  text-center hover:bg-gray-700 mb-3">
-              Learn More</nuxt-link>
+          <div class="flex w-full pb-4 pl-6 pr-6 md:inline-block">
+            <a
+              :href="show.ticketURL"
+              class="w-1/2 px-4 py-2 mb-3 mr-2 font-bold tracking-wide text-center text-white uppercase bg-blue-600 rounded-lg shadow-lg md:w-full md:block hover:bg-blue-700"
+            >
+              Buy tickets</a
+            >
+            <nuxt-link
+              :to="show.slug"
+              class="w-1/2 px-4 py-2 mb-3 font-bold tracking-wide text-center text-white uppercase bg-gray-600 rounded-lg shadow-lg md:w-full md:block hover:bg-gray-700"
+            >
+              Learn More</nuxt-link
+            >
           </div>
         </div>
       </div>
@@ -44,51 +63,52 @@
 </template>
 
 <script>
-  import AlertBox from "@/components/AlertBox";
-  import Education from "@/components/Education";
-  import Membership from "@/components/Membership";
-  import MemberCrawl from "@/components/MemberCrawl";
-  import Sponsorship from "@/components/Sponsorship";
-  import NewsWidget from "@/components/NewsWidget";
+import AlertBox from "@/components/AlertBox";
+import Education from "@/components/Education";
+import Membership from "@/components/Membership";
+import MemberCrawl from "@/components/MemberCrawl";
+import Sponsorship from "@/components/Sponsorship";
+import NewsWidget from "@/components/NewsWidget";
 
-  export default {
-    async asyncData({ $content, params }) {
-      let shows = await $content("shows", params.slug)
-        .only([
-          "title",
-          "description",
-          "img",
-          "slug",
-          "createdAt",
-          "eventDate",
-          "eventTime",
-          "formattedPrice",
-          "shortDesc",
-          "imgAlt",
-        ])
-        .sortBy("eventTime", "asc")
-        .fetch();
+export default {
+  async asyncData({ $content, params }) {
+    let shows = await $content("shows", params.slug)
+      .only([
+        "title",
+        "description",
+        "img",
+        "slug",
+        "createdAt",
+        "eventDate",
+        "eventTime",
+        "formattedPrice",
+        "shortDesc",
+        "imgAlt",
+        "ticketURL"
+      ])
+      .sortBy("eventTime", "asc")
+      .fetch();
 
-      return { shows };
-    },
-    layout: "NewLayout",
-    components: {
-      AlertBox,
-      Education,
-      Membership,
-      MemberCrawl,
-      Sponsorship,
-      NewsWidget,
-    },
-  };
+    return { shows };
+  },
+  layout: "NewLayout",
+  components: {
+    AlertBox,
+    Education,
+    Membership,
+    MemberCrawl,
+    Sponsorship,
+    NewsWidget,
+  },
+};
 </script>
 
 <style>
-  .show-card {
-    /* box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.2); */
-  }
+.show-card {
+  /* box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.2); */
+}
 
-  .default-gray {
-    background-color: #889;
-  }
+.default-gray {
+  background-color: #889;
+}
 </style>
