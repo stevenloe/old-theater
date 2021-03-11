1f0e581ae44d69f1f4c3f6a7e393f945edf86fea
Steven Loe <stevenjloe@protonmail.com>
2021-03-11 15:07:25 -0500

    add heading component

---
+++ website/components/ui/UiHeadline.vue
@@ -0,0 +1,56 @@
+ <template functional>
+  <Component
+    :is="props.tag || `h${props.level}`"
+    :ref="data.ref"
+    class="font-bold leading-tight"
+    :class="[
+      ...((props.size || props.level) == 1
+        ? ['text-3xl sm:text-4xl md:text-5xl']
+        : []),
+      ...((props.size || props.level) == 2
+        ? ['text-2xl sm:text-3xl md:text-4xl']
+        : []),
+      ...((props.size || props.level) == 3
+        ? ['text-1xl sm:text-2xl md:text-3xl']
+        : []),
+      ...((props.size || props.level) >= 4
+        ? ['text-xl sm:text-1xl md:text-2xl']
+        : []),
+      data.class,
+      data.staticClass,
+    ]"
+    :style="[data.style, data.staticStyle]"
+    v-bind="data.attrs"
+    v-on="listeners"
+  >
+    <slot />
+  </Component>
+</template>
+
+ <script>
+export const UiHeadline = {
+  name: "UiHeadline",
+  props: {
+    level: {
+      required: true,
+      type: String,
+      validator(value) {
+        // Here PurgeCSS picks up the selectors and
+        // does not remove their styles from the CSS.
+        const headlines = ["h1", "h2", "h3", "h4", "h5", "h6"];
+        return headlines.includes(`h${value}`);
+      },
+    },
+    size: {
+      default: null,
+      type: String,
+    },
+    tag: {
+      default: null,
+      type: String,
+    },
+  },
+};
+
+export default UiHeadline;
+</script>
