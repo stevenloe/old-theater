0aa12e29562cc4a986587cb5ab829613b3851234
Steven Loe <stevenjloe@protonmail.com>
2021-03-12 20:28:49 -0500

    fix: display show times correctly

--- website/plugins/format-time.js
+++ website/plugins/format-time.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 
 Vue.filter("formatTime", (date) => {
-  const options = { hour: '2-digit', minute: '2-digit' }
+  const options = { timeStyle: 'short' }
   return new Date(date).toLocaleTimeString('en-US', options)
 });
 
