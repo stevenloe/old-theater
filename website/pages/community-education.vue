15abdf9cde4f3ffb5fea2b7f89b96b86f72a3936
Steven Loe <stevenjloe@protonmail.com>
2021-03-29 17:48:23 -0400

    update content

--- website/pages/community-education.vue
+++ website/pages/community-education.vue
@@ -18,28 +18,18 @@
               alt=""
             />
             <p class="p-1 mb-3 text-xs italic text-gray-700">
-              This is caption text adipisicing elit. Vel itaque velit ipsam
-              repellat amet, earum.
+              Children's Theater Workshop participants having fun at The Old Theater.
             </p>
           </div>
           <p class="mb-4 leading-7">
-            Mauris eros sem, volutpat ut, blandit ut, pharetra quis, pede.
-            Aliquam vel libero. Suspendisse erat lacus, malesuada at, rutrum
-            posuere, scelerisque ut, ante. In non orci.
+            Our mission is to ensure that Pamlico County and the town of Oriental has a great venue for civic and cultural events.
           </p>
           <p class="mb-4 leading-7">
-            Aliquam eleifend. Aenean facilisis adipiscing urna. Maecenas
-            sagittis, velit at scelerisque ornare, elit dolor elementum ante, in
-            vestibulum elit metus sed quam. Mauris mauris ante, adipiscing
-            vitae, scelerisque vitae, luctus at, pede. Integer sagittis dui.
-            Morbi dictum mauris ac tellus. Nullam vehicula. Mauris erat neque,
-            tempor eu, posuere et, lobortis vel, enim. In mollis. Vestibulum
-            ante ipsum primis in faucibus orci luctus et ultrices posuere
-            cubilia Curae.
+            Our goal is to enrich, educate and entertain our community by serving as a venue for the performing arts. We partner with arts organizations to help them bring educational programs and theatrical, live music, and dance performances to our stage. 
           </p>
         </div>
 
-        <div class="p-8 mb-8 bg-orange-200 rounded-lg shadow-lg">
+        <div class="p-8 mb-8 bg-gray-100 rounded-lg shadow-lg">
           <UiHeadline  class="mb-4" level="3" >Pamlico Musical Society</UiHeadline>
           <div class="mb-8">
             <img
@@ -49,52 +39,37 @@
             />
           </div>
           <p class="mb-4 leading-7">
-            Mauris eros sem, volutpat ut, blandit ut, pharetra quis, pede.
-            Aliquam vel libero. Suspendisse erat lacus, malesuada at, rutrum
-            posuere, scelerisque ut, ante. In non orci.
+            Since 1997, Pamlico Musical Society has presented several concerts each year, from September through April or May. Professional artists from a variety of musical genres present the highest caliber musical entertainment, typically in the intimate setting of Oriental's Old Theater. 
           </p>
           <p class="mb-4 leading-7">
-            Aliquam eleifend. Aenean facilisis adipiscing urna. Maecenas
-            sagittis, velit at scelerisque ornare, elit dolor elementum ante, in
-            vestibulum elit metus sed quam. Mauris mauris ante, adipiscing
-            vitae, scelerisque vitae, luctus at, pede. Integer sagittis dui.
-            Morbi dictum mauris ac tellus. Nullam vehicula. Mauris erat neque,
-            tempor eu, posuere et, lobortis vel, enim. In mollis. Vestibulum
-            ante ipsum primis in faucibus orci luctus et ultrices posuere
-            cubilia Curae.
+            Jazz, classical, folk, rock, Americana, pop, and show tunes ensure that each Pamlico Musical Society season offers something for everyone who loves great music. To learn more visit <a class="underline" href="https://www.pamlicomusic.org">www.pamlicomusic.org</a>.
           </p>
         </div>
       </div>
 
       <div
-        class="p-8 mb-8 bg-pink-100 rounded-lg shadow-lg"
+        class="p-8 mb-8 bg-gray-100 rounded-lg shadow-lg"
       >
         <UiHeadline class="mb-4" level="3" >Children's Theater Workshop</UiHeadline>
-        <p class="mb-4 leading-7">
-          Mauris eros sem, volutpat ut, blandit ut, pharetra quis, pede. Aliquam
-          vel libero. Suspendisse erat lacus, malesuada at, rutrum posuere,
-          scelerisque ut, ante. In non orci.
-        </p>
-        <p class="mb-4 leading-7">
-          Aliquam eleifend. Aenean facilisis adipiscing urna. Maecenas sagittis,
-          velit at scelerisque ornare, elit dolor elementum ante, in vestibulum
-          elit metus sed quam. Mauris mauris ante, adipiscing vitae, scelerisque
-          vitae, luctus at, pede. Integer sagittis dui. Morbi dictum mauris ac
-          tellus. Nullam vehicula. Mauris erat neque, tempor eu, posuere et,
-          lobortis vel, enim. In mollis. Vestibulum ante ipsum primis in
-          faucibus orci luctus et ultrices posuere cubilia Curae.
-        </p>
-        <div class="">
+        <div class="mb-8">
           <img
             src="https://old-theater-pics.netlify.app/img/ctw.png"
             class="w-full overflow-hidden rounded-lg shadow-lg"
             alt=""
           />
         </div>
+        <p class="mb-4 leading-7">
+          Our purpose is to make the theater a setting where opportunities for telling a story in words, motion and emotions can bring awareness beyond the parameters of their own home/school environment; where life values can be imparted through drama and appreciation of social values can be learned in the play message. 
+          
+        </p>
+        <p class="mb-4 leading-7">
+          An ensemble of students learning to appreciate the power of drama techniques can display the respect, tolerance and patience for all ages, genders, social and ethnic backgrounds, as they work together toward a common goal.
+          More at <a class="underline" href="https://www.childrenstheateronc.com/">www.childrenstheateronc.com</a>
+        </p>
       </div>
 
-      <div class="p-8 pt-4 mb-8 bg-indigo-200 rounded-lg shadow-lg">
-        <UiHeadline class="mb-4" level="3" >Community Band</UiHeadline>
+      <div class="p-8 pt-4 mb-8 bg-gray-100 rounded-lg shadow-lg">
+        <UiHeadline class="mb-4" level="3" >Pamlico Community Band</UiHeadline>
         <div>
           <img
             src="https://old-theater-pics.netlify.app/img/community-band.jpg"
@@ -107,13 +82,7 @@
           </p>
         </div>
         <p class="mb-4 leading-7">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum minus
-          eligendi esse qui cupiditate neque, at, enim ipsa blanditiis vero
-          aperiam culpa labore amet corporis! Consequatur rem illum illo fuga!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-          ipsa iusto et exercitationem sed modi quod quas officiis eos ducimus,
-          repellendus molestiae consequatur harum! Aspernatur velit perspiciatis
-          veritatis tenetur dolores.
+          More at their <a class="underline" href="https://www.facebook.com/Pamlico-Community-Band-138279799519612/">Facebook page</a>
         </p>
         <p class="mb-4 leading-7">
           Mauris eros sem, volutpat ut, blandit ut, pharetra quis, pede. Aliquam
@@ -133,7 +102,7 @@
       </div>
 
       <div
-        class="p-8 pt-4 mb-8 bg-white rounded-lg shadow-lg"
+        class="p-8 pt-4 mb-8 bg-gray-100 rounded-lg shadow-lg"
       >
 
         <UiHeadline class="mb-4" level="3" >Pamlico Chorale</UiHeadline>
@@ -149,12 +118,7 @@
           </p>
         </div>
         <p class="mb-4 leading-7">
-          Eligendi esse qui cupiditate neque, at, enim ipsa blanditiis vero
-          aperiam culpa labore amet corporis! Consequatur rem illum illo fuga!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-          ipsa iusto et exercitationem sed modi quod quas officiis eos ducimus,
-          repellendus molestiae consequatur harum! Aspernatur velit perspiciatis
-          veritatis tenetur dolores.
+          <a class="underline" href="https://www.facebook.com/Pamlico-Chorale-158261794372262/">Pamlico Chorale Facebook page</a>
         </p>
         <p class="mb-4 leading-7">
           Mauris eros sem, volutpat ut, blandit ut, pharetra quis, pede. Aliquam
