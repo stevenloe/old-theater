08f1ca960e9ea5c545fb057d0f89ff2b1244a684
Steven Loe <stevenjloe@protonmail.com>
2021-05-25 14:55:34 -0400

    add new membership page

---
+++ website/pages/support/Membership.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="mx-auto">
+    <div
+      class="w-full mx-auto md:w-3/4 lg:w-9/12 xl:3/4 lg:text-xl xl:text-2xl"
+    >
+      <UiHeadline id="level-1-headline" level="1" @click="shout('h1')"> Join the Old Theater</UiHeadline>
+      <article class="p-6 mt-6 bg-white rounded shadow lg:p-8">
+        <div
+          class="mx-auto mb-8 prose-sm prose sm:prose lg:prose-lg xl:prose-2xl"
+        >
+          <p>
+            Oriental’s Old Theater first opened its doors as a movie theater in
+            1946, and served as a social entertainment center until the late
+            50s. It was revitalized and reopened in 1995, providing a venue
+            where performing arts can thrive in Pamlico County.
+          </p>
+          <p>
+            The Old Theater is owned and managed by the Old Theater Corporation,
+            a registered 501(c)(3) non-profit organization. All memberships and
+            donations are tax deductible. Its Board is comprised of Pamlico
+            County volunteers whose mission is to ensure that Pamlico County and
+            the town of Oriental has a venue for civic and cultural events. The
+            ability to achieve this mission depends on the ongoing support and
+            participation of individuals, businesses, service organizations,
+            grants and volunteers on stage and behind the scenes.
+          </p>
+          <p>
+            Thanks to the generosity of people like YOU, significant
+            improvements have been accomplished over the past few years. A
+            successful 2021 membership drive will enable us to continue needed
+            improvements and the variety of entertainment our community has come
+            to enjoy.
+          </p>
+          <p>
+            We need and welcome your financial support and look forward to
+            hearing from you. If you are a current member of the Old Theater, we
+            thank you again for your generous support and hope we can count on
+            it for 2021. If you are a new member for 2021, thank you and
+            welcome! We look forward to seeing everyone throughout the coming
+            year.
+          </p>
+          <p>
+            For more information email
+            <a href="mailto:OldTheater@gmail.com">OldTheater@gmail.com</a>. To
+            join now, click HERE: [2021 Membership Form](#)
+          </p>
+          <p><div class="font-bold">
+            Let the show go on!
+          </div>
+
+          <span class="ml-4 italic">Izzy Keppner, President </span>
+             
+             <div class="ml-4 italic">
+               
+             Andrea Bayard, Membership
+            & Volunteer Manager
+             </div>
+          </p>
+          <p>
+            Volunteers are the life blood of our organization. Please consider
+            volunteering your time and talents so the Old Theater may continue
+            being the “center stage” in Pamlico County. <nuxt-link to="/volunteer/volunteers-we-need-you">Learn more about volunteering</nuxt-link>
+          </p>
+
+          <member-list></member-list>
+        </div>
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+import UiHeadline from "@/components/ui/UiHeadline";
+import MemberList from "@/components/MemberList";
+export default {
+  async asyncData({ $content, params }) {
+    const content = await $content("supports", params.slug).fetch();
+    return { content };
+  },
+  layout: "NewLayout",
+  methods: {
+    shout(val) {
+      console.log("Shout", val);
+    },
+  },
+  components: {
+    MemberList,
+  },
+};
+</script>
---
+++ website/pages/volunteer/Volunteers-We-Need-You.vue
@@ -0,0 +1,513 @@
+<template>
+  <div class="mx-auto">
+    <div
+      class="w-full mx-auto content-column md:w-3/4 lg:w-9/12 xl:3/4 lg:text-xl xl:text-2xl"
+    >
+      <UiHeadline level="1" class="mb-6">Get Involved</UiHeadline>
+      <div class="p-8 bg-white rounded shadow-lg">
+        <div class="mb-4">
+          <div>
+            <UiHeadline level="2" class="mb-8"
+              >Come Join Us! Be Part of the Old Theater!</UiHeadline
+            >
+
+            <img
+              src="http://via.placeholder.com/400x200"
+              class="w-full mr-3"
+              alt=""
+            />
+            <p class="p-1 mb-3 text-xs italic text-gray-700 bg-gray-200">
+              Community theatre is made possible because of community
+              involvement and support.
+            </p>
+          </div>
+          <p class="my-4 text-gray-800">
+            For decades, The Old Theater has been bringing the best in movies,
+            music, and community theater to Pamlico county. Driven by the
+            passion and energy of its volunteers, the Old Theater brings
+            together community residents to present quality entertainment and
+            moving arts experiences for all ages.
+          </p>
+          <UiHeadline level="3" class="my-4">We Need Volunteers</UiHeadline>
+          <p>
+            The Old Theater provides a stage where our diverse community can
+            come together to experience and celebrate the joys of theater. But
+            our building would be a dark and lonely place if not for people who
+            give the generous gift of their time and energy to bring events to
+            life. Our volunteers are united by their passion for the performing
+            arts. They appreciate how special it is for a town the size of
+            Oriental to have a functioning theater where such a variety of
+            entertainment is presented to its community.
+          </p>
+          <UiHeadline level="3" class="my-4">Play Your Part</UiHeadline>
+          <p class="my-4">
+            Our success - past, present, and future - is dependent on the
+            dedication of our volunteers whose time, talents and spirit continue
+            to bring the quality entertainment that The Old Theater prides
+            itself on. Many faces truly bring our theatre to life - booking
+            touring acts, selling tickets, planning fundraisers, planning shows
+            & rehearsals, running sound &amp; lights, serving popcorn, sweeping
+            floors and shining on stage. We welcome volunteers from throughout
+            the Community.
+          </p>
+          <p class="my-4">
+            As one of our volunteers, you’ll serve as a goodwill ambassador for
+            the Old Theater and will have fun working on outstanding events and
+            productions. No matter what your experience level, the Old Theater
+            has opportunities for you to get involved! Tell us how you’d like to
+            participate in a theater-sponsored event by filling out the
+            following online form, via snail mail by clicking here <a class="underline" href="../pdf/volunteer_form.pdf">Volunteer Form.pdf</a> or by email to oldtheater@gmail.com.
+          </p>
+          <p class="my-4 font-semibold">
+            Hope to hear from you!
+          </p>
+
+        </div>
+        <form
+          method="post"
+          id="zcr009d1cac128b8d3a3c51ca4fca706a52"
+          class="zemContactForm"
+          action="/volunteer/volunteers-we-need-you#zcr009d1cac128b8d3a3c51ca4fca706a52"
+        >
+          <div class="p-8 border border-gray-400">
+            <input
+              type="hidden"
+              name="zem_contact_nonce"
+              value="559645a1d8b2d336f1f9705c6b206a76"
+            />
+            <input
+              type="hidden"
+              name="zem_contact_form_id"
+              value="009d1cac128b8d3a3c51ca4fca706a52"
+            />
+            <label for="Email" class="zemText zemRequired Email">Email</label
+            ><br /><input
+              type="text"
+              id="Email"
+              class="zemText zemRequired"
+              name="Email"
+              value=""
+              size="40"
+              maxlength="100"
+            /><br />
+            <label for="Name" class="zemText zemRequired Name">Name</label
+            ><br /><input
+              type="text"
+              id="Name"
+              class="zemText zemRequired"
+              name="Name"
+              value=""
+              size="40"
+              maxlength="100"
+            /><br />
+            <label for="Address" class="zemText zemRequired Address"
+              >Address</label
+            ><br /><input
+              type="text"
+              id="Address"
+              class="border border-gray-300"
+              name="Address"
+              value=""
+              size="40"
+              maxlength="100"
+            /><br />
+            <label for="City" class="zemText zemRequired City">City</label
+            ><br /><input
+              type="text"
+              id="City"
+              class="zemText zemRequired"
+              name="City"
+              value=""
+              maxlength="100"
+            /><br />
+            <label
+              for="q5e178542b85fb18ca3c459e9a95f4f2e"
+              class="zemText zemRequired q5e178542b85fb18ca3c459e9a95f4f2e"
+              >Zip Code</label
+            ><br /><input
+              type="text"
+              id="q5e178542b85fb18ca3c459e9a95f4f2e"
+              class="zemText zemRequired"
+              name="q5e178542b85fb18ca3c459e9a95f4f2e"
+              value=""
+              maxlength="100"
+            /><br />
+            <label for="Phone" class="zemText zemRequired Phone">Phone</label
+            ><br /><input
+              type="text"
+              id="Phone"
+              class="zemText zemRequired"
+              name="Phone"
+              value=""
+              maxlength="100"
+            /><br />
+            <hr />
+            Your Interests (check all that apply):<br />
+            <input
+              type="checkbox"
+              id="Acting"
+              class="zemCheckbox"
+              name="Acting"
+            />
+            <label for="Acting" class="zemCheckbox Acting">Acting</label><br />
+            <input
+              type="checkbox"
+              id="Production"
+              class="zemCheckbox"
+              name="Production"
+            />
+            <label for="Production" class="zemCheckbox Production"
+              >Production</label
+            ><br />
+            <input
+              type="checkbox"
+              id="Director"
+              class="zemCheckbox"
+              name="Director"
+            />
+            <label for="Director" class="zemCheckbox Director">Director</label
+            ><br />
+            <input
+              type="checkbox"
+              id="q7d25ea95e7e29f0e53fb5149cc383c1d"
+              class="zemCheckbox"
+              name="q7d25ea95e7e29f0e53fb5149cc383c1d"
+            />
+            <label
+              for="q7d25ea95e7e29f0e53fb5149cc383c1d"
+              class="zemCheckbox q7d25ea95e7e29f0e53fb5149cc383c1d"
+              >Stage Manager/Assistant Stage Manager</label
+            ><br />
+            <input
+              type="checkbox"
+              id="q85b5efd9aa41e61cb2fc4733a8c431fb"
+              class="zemCheckbox"
+              name="q85b5efd9aa41e61cb2fc4733a8c431fb"
+            />
+            <label
+              for="q85b5efd9aa41e61cb2fc4733a8c431fb"
+              class="zemCheckbox q85b5efd9aa41e61cb2fc4733a8c431fb"
+              >Light Board Operator</label
+            ><br />
+            <input
+              type="checkbox"
+              id="qd2668b10e909dd789973733ba03c5746"
+              class="zemCheckbox"
+              name="qd2668b10e909dd789973733ba03c5746"
+            />
+            <label
+              for="qd2668b10e909dd789973733ba03c5746"
+              class="zemCheckbox qd2668b10e909dd789973733ba03c5746"
+              >Sound Board Operator</label
+            ><br />
+            <input
+              type="checkbox"
+              id="qe81511041ebc711cdc6e9f9563d9a47d"
+              class="zemCheckbox"
+              name="qe81511041ebc711cdc6e9f9563d9a47d"
+            />
+            <label
+              for="qe81511041ebc711cdc6e9f9563d9a47d"
+              class="zemCheckbox qe81511041ebc711cdc6e9f9563d9a47d"
+              >Set Design</label
+            ><br />
+            <input
+              type="checkbox"
+              id="qd6ed0cb399e1635f8866c397e4ce4b55"
+              class="zemCheckbox"
+              name="qd6ed0cb399e1635f8866c397e4ce4b55"
+            />
+            <label
+              for="qd6ed0cb399e1635f8866c397e4ce4b55"
+              class="zemCheckbox qd6ed0cb399e1635f8866c397e4ce4b55"
+              >Set Construction/Strike, Painting</label
+            ><br />
+            <input
+              type="checkbox"
+              id="Props"
+              class="zemCheckbox"
+              name="Props"
+            />
+            <label for="Props" class="zemCheckbox Props">Props</label><br />
+            <input
+              type="checkbox"
+              id="Wardrobe"
+              class="zemCheckbox"
+              name="Wardrobe"
+            />
+            <label for="Wardrobe" class="zemCheckbox Wardrobe">Wardrobe</label
+            ><br />
+            <input
+              type="checkbox"
+              id="Makeup"
+              class="zemCheckbox"
+              name="Makeup"
+            />
+            <label for="Makeup" class="zemCheckbox Makeup">Makeup</label><br />
+            <input
+              type="checkbox"
+              id="Stagehand"
+              class="zemCheckbox"
+              name="Stagehand"
+            />
+            <label for="Stagehand" class="zemCheckbox Stagehand"
+              >Stagehand</label
+            ><br />
+            <input
+              type="checkbox"
+              id="q20165de7491435a41583d2c3686dd55c"
+              class="zemCheckbox"
+              name="q20165de7491435a41583d2c3686dd55c"
+            />
+            <label
+              for="q20165de7491435a41583d2c3686dd55c"
+              class="zemCheckbox q20165de7491435a41583d2c3686dd55c"
+              >Movie Projection</label
+            ><br />
+            <input
+              type="checkbox"
+              id="q49a368e9b39f9674178faed0956eb76e"
+              class="zemCheckbox"
+              name="q49a368e9b39f9674178faed0956eb76e"
+            />
+            <label
+              for="q49a368e9b39f9674178faed0956eb76e"
+              class="zemCheckbox q49a368e9b39f9674178faed0956eb76e"
+              >Ticket Window</label
+            ><br />
+            <input
+              type="checkbox"
+              id="qb921127f9ea37f6524eeb78541b46646"
+              class="zemCheckbox"
+              name="qb921127f9ea37f6524eeb78541b46646"
+            />
+            <label
+              for="qb921127f9ea37f6524eeb78541b46646"
+              class="zemCheckbox qb921127f9ea37f6524eeb78541b46646"
+              >Greeter, Concession Sales, Usher</label
+            ><br />
+            <input
+              type="checkbox"
+              id="qbf420776af6961ba792a744f979e25b0"
+              class="zemCheckbox"
+              name="qbf420776af6961ba792a744f979e25b0"
+            />
+            <label
+              for="qbf420776af6961ba792a744f979e25b0"
+              class="zemCheckbox qbf420776af6961ba792a744f979e25b0"
+              >Administrative Support</label
+            ><br />
+            <input
+              type="checkbox"
+              id="q0a3d0b04e427240e8e16f932c8c7f847"
+              class="zemCheckbox"
+              name="q0a3d0b04e427240e8e16f932c8c7f847"
+            />
+            <label
+              for="q0a3d0b04e427240e8e16f932c8c7f847"
+              class="zemCheckbox q0a3d0b04e427240e8e16f932c8c7f847"
+              >Facility Enhancement</label
+            ><br />
+            <hr />
+            Availability (check all that apply):<br />
+            <input
+              type="checkbox"
+              id="Weekdays"
+              class="zemCheckbox"
+              name="Weekdays"
+            />
+            <label for="Weekdays" class="zemCheckbox Weekdays">Weekdays</label
+            ><br />
+            <input
+              type="checkbox"
+              id="Weekends"
+              class="zemCheckbox"
+              name="Weekends"
+            />
+            <label for="Weekends" class="zemCheckbox Weekends">Weekends</label
+            ><br />
+            <input
+              type="checkbox"
+              id="Mornings"
+              class="zemCheckbox"
+              name="Mornings"
+            />
+            <label for="Mornings" class="zemCheckbox Mornings">Mornings</label
+            ><br />
+            <input
+              type="checkbox"
+              id="Afternoons"
+              class="zemCheckbox"
+              name="Afternoons"
+            />
+            <label for="Afternoons" class="zemCheckbox Afternoons"
+              >Afternoons</label
+            ><br />
+            <input
+              type="checkbox"
+              id="Evenings"
+              class="zemCheckbox"
+              name="Evenings"
+            />
+            <label for="Evenings" class="zemCheckbox Evenings">Evenings</label
+            ><br />
+            <hr />
+            <label
+              for="q707ee00657f16ee7dda23657f89936a2"
+              class="zemTextarea q707ee00657f16ee7dda23657f89936a2"
+              >Comments, Other Info:</label
+            ><br /><textarea
+              id="q707ee00657f16ee7dda23657f89936a2"
+              class="zemTextarea"
+              name="q707ee00657f16ee7dda23657f89936a2"
+              cols="78"
+              rows="8"
+            ></textarea
+            ><br />
+            <br />
+            <input
+              type="submit"
+              class="zemSubmit"
+              name="zem_contact_submit"
+              value="Submit"
+            />
+          </div>
+        </form>
+
+        <div class="mb-4">
+          <div class="flex bg-orange-200">
+            <div class="w-1/2 m-4">
+              <img
+                src="http://via.placeholder.com/400x200"
+                class="w-full"
+                alt=""
+              />
+            </div>
+            <div class="w-1/2 m-4 ml-0">
+              <img
+                src="http://via.placeholder.com/400x200"
+                class="w-full"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="mb-4">
+          <h2 class="text-2xl font-semibold"></h2>
+          <p class="mb-4">
+            Nostrum minus eligendi esse qui cupiditate neque, at, enim ipsa
+            blanditiis vero aperiam culpa labore amet corporis! Consequatur rem
+            illum illo fuga! Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Accusamus ipsa iusto et exercitationem sed modi quod quas
+            officiis eos ducimus, repellendus molestiae consequatur harum!
+            Aspernatur velit perspiciatis veritatis tenetur dolores.
+          </p>
+          <p class="mb-4">
+            Mauris eros sem, volutpat ut, blandit ut, pharetra quis, pede.
+            Aliquam vel libero. Suspendisse erat lacus, malesuada at, rutrum
+            posuere, scelerisque ut, ante. In non orci.
+          </p>
+          <div class="flex">
+            <div class="w-1/3 m-2">
+              <img
+                src="http://via.placeholder.com/400x200"
+                class="w-full"
+                alt=""
+              />
+            </div>
+            <div class="w-1/3 m-2">
+              <img
+                src="http://via.placeholder.com/400x200"
+                class="w-full"
+                alt=""
+              />
+            </div>
+            <div class="w-1/3 m-2">
+              <img
+                src="http://via.placeholder.com/400x200"
+                class="w-full"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="p-8 pt-4 mb-4 bg-indigo-100">
+          <h2 class="mb-1 text-2xl font-semibold">Volunteers in Action</h2>
+          <div>
+            <img
+              src="http://via.placeholder.com/600x300"
+              class="w-full mr-3"
+              alt=""
+            />
+            <p class="p-1 mb-3 text-xs italic bg-blue-300 text-blue">
+              This is caption text adipisicing elit. Vel itaque velit ipsam
+              repellat amet, earum.
+            </p>
+          </div>
+          <p class="mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            minus eligendi esse qui cupiditate neque, at, enim ipsa blanditiis
+            vero aperiam culpa labore amet corporis! Consequatur rem illum illo
+            fuga! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Accusamus ipsa iusto et exercitationem sed modi quod quas officiis
+            eos ducimus, repellendus molestiae consequatur harum! Aspernatur
+            velit perspiciatis veritatis tenetur dolores.
+          </p>
+          <p class="mb-4">
+            Aliquam eleifend. Aenean facilisis adipiscing urna. Maecenas
+            sagittis, velit at scelerisque ornare, elit dolor elementum ante, in
+            vestibulum elit metus sed quam. Mauris mauris ante, adipiscing
+            vitae, scelerisque vitae, luctus at, pede. Integer sagittis dui.
+            Morbi dictum mauris ac tellus. Nullam vehicula. Mauris erat neque,
+            tempor eu, posuere et, lobortis vel, enim. In mollis. Vestibulum
+            ante ipsum primis in faucibus orci luctus et ultrices posuere
+            cubilia Curae.
+          </p>
+          <div class="flex"></div>
+        </div>
+
+        <div class="p-8 pt-4 mb-4">
+          <h2 class="text-2xl font-semibold">We Need You!</h2>
+          <div class="float-right ml-2">
+            <img src="http://via.placeholder.com/350x220" alt="" />
+            <p class="p-1 mb-3 text-xs italic text-blue">
+              Aliquam vel libero. Suspendisse erat lacus.
+            </p>
+          </div>
+          <p class="mb-4">
+            Eligendi esse qui cupiditate neque, at, enim ipsa blanditiis vero
+            aperiam culpa labore amet corporis! Consequatur rem illum illo fuga!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+            ipsa iusto et exercitationem sed modi quod quas officiis eos
+            ducimus, repellendus molestiae consequatur harum! Aspernatur velit
+            perspiciatis veritatis tenetur dolores.
+          </p>
+          <p class="mb-4">
+            Mauris eros sem, volutpat ut, blandit ut, pharetra quis, pede.
+            Aliquam vel libero. Suspendisse erat lacus, malesuada at, rutrum
+            posuere, scelerisque ut, ante. In non orci.
+          </p>
+        </div>
+        <div class="m-4 md:m-8">
+          <MemberCrawl />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MemberCrawl from "@/components/MemberCrawl";
+
+export default {
+  metaInfo: {
+    title: "Get  Involved",
+  },
+  components: {
+    MemberCrawl,
+  },
+  layout: "NewLayout",
+};
+</script>
