8fb5f5e8bb39cdfb7c1584c4259588504d79eef5
Steven Loe <stevenjloe@protonmail.com>
2021-05-25 12:42:15 -0400

    update links

--- website/components/NavBar.vue
+++ website/components/NavBar.vue
@@ -191,7 +191,7 @@ export default {
       aboutMenuItems: {
         title: "About",
         items: [
-          { text: "About Us", link: "/about/The Old Theater/", id: 0 },
+          { text: "About Us", link: "/about/the-old-theater/", id: 0 },
           { text: "Venue", link: "/about/venue/", id: 1 },
           { text: "Rent the Theater", link: "/about/rent-the-theater", id: 2 },
           { text: "Our History", link: "/about/history", id: 3 },
