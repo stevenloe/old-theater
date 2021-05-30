973b779c56ebafed39ca44aedc89dc7d11d87cbe
Steven Loe <stevenjloe@protonmail.com>
2021-05-30 14:38:30 -0400

    pass nuxt-content as prop

---
+++ website/components/PropMe.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>This is Prop Me {{ }}</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    peeps: {
+      type: Object,
+      required: true,
+      default: {}
+    },
+    memberData: {
+        type: Array,
+        default: () => []
+    }
+  },
+  mounted() {
+    console.log("MOUNTED PROP ME", this.peeps[0].body);
+    console.log("MOUNTED: PROP ME :: memberData", this.memberData);
+  },
+}
+</script>
