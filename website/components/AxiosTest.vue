ac42de019cce3d41edd99a79551fbc57b7560af8
Steven Loe <stevenjloe@protonmail.com>
2021-06-07 12:15:28 -0400

    update test page

--- website/components/AxiosTest.vue
+++ website/components/AxiosTest.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="px-6 py-64 bg-blue-200">
     AXIOS TEST COMPONENT
+
+    <ul id="example-1">
+  <li v-for="post in posts" :key="post.id">
+    <h2 class="mt-4 font-bold">
+      {{ post.title }}
+      </h2>
+      <p>{{post.description}}</p>
+      
+  </li>
+</ul>
   </div>
 
 
@@ -11,6 +21,21 @@
 export default {
   created() {
     console.log("---- CREATED AXIOS TEST COMPONENT ------");
-  }
+  },
+  // async fetch() {
+  //     this.mountains = await fetch(
+  //       'https://api.nuxtjs.dev/mountains'
+  //     ).then(res => res.json())
+  //   }
+  data() {
+    return {
+      posts: []
+    }
+  },
+  async fetch() {
+    this.posts = await fetch('https://api.nuxtjs.dev/posts').then(res =>
+      res.json()
+    )
+  },
 }
 </script>
