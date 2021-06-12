0debfe5b797c7537e71bf04a7a4024f869e567e9
Steven Loe <stevenjloe@protonmail.com>
2021-06-11 21:57:54 -0400

    fix: add missing imports

--- website/pages/community-education.vue
+++ website/pages/community-education.vue
@@ -4,11 +4,10 @@
       class="w-full mx-auto content-column md:w-3/4 lg:w-9/12 xl:3/4 lg:text-xl xl:text-2xl"
     >
       <div class="mb-8">
-
-        <UiHeadline level="1" @click="shout('h1')">Community &amp; Education</UiHeadline>
-        <h1 class="mt-4 mb-4 text-4xl font-extrabold">
-          
-        </h1>
+        <UiHeadline level="1" @click="shout('h1')"
+          >Community &amp; Education</UiHeadline
+        >
+        <h1 class="mt-4 mb-4 text-4xl font-extrabold"></h1>
 
         <div class="p-8 mb-8 bg-white rounded-lg shadow-lg">
           <div>
@@ -18,11 +17,16 @@
               alt=""
             />
             <p class="p-1 mb-3 text-sm italic text-gray-700">
-              Children's Theater Workshop participants having fun at The Old Theater.
+              Children's Theater Workshop participants having fun at The Old
+              Theater.
             </p>
           </div>
           <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
-            The Old Theater's goal is to enrich, educate and entertain our community by serving as a great venue for the performing arts. We partner with arts organizations to help them bring educational programs and theatrical, live music, and dance performances to our stage. 
+            The Old Theater's goal is to enrich, educate and entertain our
+            community by serving as a great venue for the performing arts. We
+            partner with arts organizations to help them bring educational
+            programs and theatrical, live music, and dance performances to our
+            stage.
           </p>
           <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
             Below are some of the organizations we partner with.
@@ -30,7 +34,9 @@
         </div>
 
         <div class="p-8 mb-8 bg-orange-200 rounded-lg shadow-lg">
-          <UiHeadline  class="mb-4" level="3" >Pamlico Musical Society</UiHeadline>
+          <UiHeadline class="mb-4" level="3"
+            >Pamlico Musical Society</UiHeadline
+          >
           <div class="mb-8">
             <img
               src="https://old-theater-pics.netlify.app/img/pms.png"
@@ -39,19 +45,30 @@
             />
           </div>
           <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
-            Since 1997, Pamlico Musical Society has presented several concerts each year, from September through April or May. Professional artists from a variety of musical genres present the highest caliber musical entertainment, typically in the intimate setting of Oriental's Old Theater. 
+            Since 1997, Pamlico Musical Society has presented several concerts
+            each year, from September through April or May. Professional artists
+            from a variety of musical genres present the highest caliber musical
+            entertainment, typically in the intimate setting of Oriental's Old
+            Theater.
           </p>
           <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
-            Jazz, classical, folk, rock, Americana, pop, and show tunes ensure that each Pamlico Musical Society season offers something for everyone who loves great music. To learn more visit <a class="underline" href="https://www.pamlicomusic.org">www.pamlicomusic.org</a>.
+            Jazz, classical, folk, rock, Americana, pop, and show tunes ensure
+            that each Pamlico Musical Society season offers something for
+            everyone who loves great music. To learn more visit
+            <a class="underline" href="https://www.pamlicomusic.org"
+              >www.pamlicomusic.org</a
+            >.
           </p>
         </div>
       </div>
 
-      <div
-        class="p-8 mb-8 bg-pink-100 rounded-lg shadow-lg"
-      >
-        <UiHeadline class="mb-2" level="3" >Children's Theater Workshop</UiHeadline>
-        <UiHeadline class="mb-4" level="5" >A Place for Kids to Learn, Grow and Imagine.</UiHeadline>
+      <div class="p-8 mb-8 bg-pink-100 rounded-lg shadow-lg">
+        <UiHeadline class="mb-2" level="3"
+          >Children's Theater Workshop</UiHeadline
+        >
+        <UiHeadline class="mb-4" level="5"
+          >A Place for Kids to Learn, Grow and Imagine.</UiHeadline
+        >
         <div class="mb-8">
           <img
             src="https://old-theater-pics.netlify.app/img/ctw.png"
@@ -59,23 +76,36 @@
             alt=""
           />
         </div>
-        
 
         <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
-          Children's Theater Workshop has a longstanding commitment to enriching the lives of children by providing a structured arts program led by an exceptional, and carefully chosen, theater arts Drama Coach.
+          Children's Theater Workshop has a longstanding commitment to enriching
+          the lives of children by providing a structured arts program led by an
+          exceptional, and carefully chosen, theater arts Drama Coach.
         </p>
 
         <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
-          Besides being great fun, children's theatre has many benefits. Activities like improv and drama performance can boost self-esteem and confidence. The theatre is a place where inner silliness and creativity are embraced. When children learn to be more confident in themselves, it translates to other aspects of their lives &mdash; school, communication, and social situations.
+          Besides being great fun, children's theatre has many benefits.
+          Activities like improv and drama performance can boost self-esteem and
+          confidence. The theatre is a place where inner silliness and
+          creativity are embraced. When children learn to be more confident in
+          themselves, it translates to other aspects of their lives &mdash;
+          school, communication, and social situations.
         </p>
         <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
-          In addition to boosting confidence and creativity, Children's Theater Workshop has many other benefits. If your child is in the workshop, chances are they’ll have to learn part of a script. When they see what they read come to life on the stage, they’ll likely improve their reading comprehension elsewhere. It might even make them more interested in reading off the stage, too!
-          More at <a class="underline" href="https://www.childrenstheateronc.com/">www.childrenstheateronc.com</a>
+          In addition to boosting confidence and creativity, Children's Theater
+          Workshop has many other benefits. If your child is in the workshop,
+          chances are they’ll have to learn part of a script. When they see what
+          they read come to life on the stage, they’ll likely improve their
+          reading comprehension elsewhere. It might even make them more
+          interested in reading off the stage, too! More at
+          <a class="underline" href="https://www.childrenstheateronc.com/"
+            >www.childrenstheateronc.com</a
+          >
         </p>
       </div>
 
       <div class="p-8 pt-4 mb-8 bg-indigo-300 rounded-lg shadow-lg">
-        <UiHeadline class="mb-4" level="3" >Pamlico Community Band</UiHeadline>
+        <UiHeadline class="mb-4" level="3">Pamlico Community Band</UiHeadline>
         <div>
           <img
             src="https://old-theater-pics.netlify.app/img/community-band.jpg"
@@ -87,21 +117,26 @@
           </p>
         </div>
         <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
-          The Pamlico Community Band has members from Pamlico County and surrounding counties. Ages range from the young, to the young at heart! It doesn't matter what experience you have. Only thing that matters, is that you have a passion for music!
+          The Pamlico Community Band has members from Pamlico County and
+          surrounding counties. Ages range from the young, to the young at
+          heart! It doesn't matter what experience you have. Only thing that
+          matters, is that you have a passion for music!
         </p>
         <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
-          More at their <a class="underline" href="https://www.facebook.com/Pamlico-Community-Band-138279799519612/">Facebook page</a>
+          More at their
+          <a
+            class="underline"
+            href="https://www.facebook.com/Pamlico-Community-Band-138279799519612/"
+            >Facebook page</a
+          >
         </p>
-     
+
         <div class="flex"></div>
       </div>
 
-      <div
-        class="p-8 pt-4 mb-8 bg-gray-100 rounded-lg shadow-lg"
-      >
+      <div class="p-8 pt-4 mb-8 bg-gray-100 rounded-lg shadow-lg">
+        <UiHeadline class="mb-4" level="3">Pamlico Chorale</UiHeadline>
 
-        <UiHeadline class="mb-4" level="3" >Pamlico Chorale</UiHeadline>
-        
         <div class="float-right ml-2">
           <img
             src="https://old-theater-pics.netlify.app/img/chorale.jpg"
@@ -113,22 +148,30 @@
           </p>
         </div>
         <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
-          The Pamlico Chorale is an established choral group in Pamlico County, North Carolina. Its members enjoy singing and rejoicing together. Reba Tiller is the group's beloved, long-standing leader. Participation is open to all who share a love of music.
+          The Pamlico Chorale is an established choral group in Pamlico County,
+          North Carolina. Its members enjoy singing and rejoicing together. Reba
+          Tiller is the group's beloved, long-standing leader. Participation is
+          open to all who share a love of music.
         </p>
         <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
-          More at their  <a class="underline" href="https://www.facebook.com/Pamlico-Chorale-158261794372262/"> Facebook page</a>
+          More at their
+          <a
+            class="underline"
+            href="https://www.facebook.com/Pamlico-Chorale-158261794372262/"
+          >
+            Facebook page</a
+          >
         </p>
-  
       </div>
       <client-only>
-        <member-crawl />
+        <member-carousel />
       </client-only>
     </div>
   </div>
 </template>
 
 <script>
-import MemberCrawl from "@/components/MemberCrawl";
+import MemberCarousel from "@/components/MemberCarousel";
 import UiHeadline from "@/components/ui/UiHeadline";
 
 export default {
@@ -136,15 +179,27 @@ export default {
     title: "About us",
   },
   components: {
-    MemberCrawl,
-    UiHeadline 
+    MemberCarousel,
+    UiHeadline,
+    MemberCarousel,
   },
   layout: "NewLayout",
+
+  async fetch() {
+
+
+
+    this.members = await fetch(
+      "https://old-theater.netlify.app/data/members2.json"
+    ).then((res) => res.json());
+  },
+
+
 };
 </script>
 
 <style scoped>
-  a:hover {
-    color:#555;
-  }
+a:hover {
+  color: #555;
+}
 </style>
