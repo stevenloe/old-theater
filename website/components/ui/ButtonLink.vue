395fddce20de2dcadc95653ef31d767a897c0855
Steven Loe <stevenjloe@protonmail.com>
2021-06-15 12:53:09 -0400

    ui: update button link link style

--- website/components/ui/ButtonLink.vue
+++ website/components/ui/ButtonLink.vue
@@ -1,5 +1,9 @@
 <template>
-  <nuxt-link :to='url' class="block px-4 py-2 mb-3 text-lg font-semibold tracking-wide text-center text-gray-700 uppercase bg-white border-2 border-gray-700 rounded-lg hover:shadow-md hover:border-black hover:text-white hover:bg-indigo-200 sm:w-1/3">{{ text }}</nuxt-link>
+  <nuxt-link :to='url'>
+    <div class="block px-4 py-2 mb-3 text-lg font-semibold tracking-wide text-center text-gray-900 uppercase bg-white border-2 border-gray-700 rounded-lg hover:text-white hover:shadow-md hover:border-black hover:bg-black sm:w-1/3">
+    {{ text }}
+    </div>
+    </nuxt-link>
 </template>
 
 <script>
