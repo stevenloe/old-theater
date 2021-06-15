61f4eb591be001268b8945432f1421f05c1345b5
Steven Loe <stevenjloe@protonmail.com>
2021-06-14 21:16:45 -0400

    fix: broken links

--- website/components/Sponsorship.vue
+++ website/components/Sponsorship.vue
@@ -8,7 +8,7 @@
     <div class="pt-3 pb-4 pl-6 pr-6 bg-purple-800 rounded-b-lg shadow-lg ">
       <h2 class="text-4xl font-bold text-white text-shadow">{{ item.title }}</h2>
       <p class="block mb-5 text-lg font-medium text-white md:text-2xl lg:text-3xl">{{ item.description }}</p>
-      <nuxt-link to="/support" class="block px-4 py-2 mb-3 text-lg font-semibold tracking-wide text-center text-white uppercase bg-blue-600 border-2 border-white rounded-lg shadow-md hover:bg-blue-700 sm:w-1/3">{{ item.cta }}</nuxt-link>
+      <nuxt-link to="/support/sponsor" class="block px-4 py-2 mb-3 text-lg font-semibold tracking-wide text-center text-white uppercase bg-blue-600 border-2 border-white rounded-lg shadow-md hover:bg-blue-700 sm:w-1/3">{{ item.cta }}</nuxt-link>
     </div>
   </div>
 </template>
