01b08f70492b3ebb51f06ea7b9281842e2e0e9c9
Steven Loe <stevenjloe@protonmail.com>
2021-06-16 13:56:07 -0400

    volunteer form revisions

--- website/pages/volunteer-form.vue
+++ website/pages/volunteer-form.vue
@@ -1,311 +1,194 @@
 <template>
-  <form
-    method="post"
-    id="zcr009d1cac128b8d3a3c51ca4fca706a52"
-    class="zemContactForm"
-    action="/volunteer/volunteers-we-need-you#zcr009d1cac128b8d3a3c51ca4fca706a52"
-   
-  >
-    <div class="p-8 border border-gray-400">
-      <h1 class="mb-1 text-3xl font-bold">Old Theater Volunteer Form</h1>
-      <h2 class="text-2xl font-semibold">We Need You!</h2>
-      <p class="max-w-3xl mb-6 text-xl">As one of our volunteers, you’ll serve as a goodwill ambassador for
-            the Old Theater and will have fun working on outstanding events and
-            productions. No matter what your experience level, the Old Theater
-            has opportunities for you to get involved! Tell us how you’d like to
-            participate in a theater-sponsored event by filling out this form.
-           </p>
-      <input
-        type="hidden"
-        name="zem_contact_nonce"
-        value="559645a1d8b2d336f1f9705c6b206a76"
-      />
-      <input
-        type="hidden"
-        name="zem_contact_form_id"
-        value="009d1cac128b8d3a3c51ca4fca706a52"
-      />
-      <label for="Email" class="zemText zemRequired Email">Email</label
-      ><br /><input
-        type="text"
-        id="Email"
-        class="zemText zemRequired"
-        name="Email"
-        value=""
-        size="40"
-        maxlength="100"
-      /><br />
-      <label for="Name" class="zemText zemRequired Name">Name</label
-      ><br /><input
-        type="text"
-        id="Name"
-        class="zemText zemRequired"
-        name="Name"
-        value=""
-        size="40"
-        maxlength="100"
-      /><br />
-      <label for="Address" class="zemText zemRequired Address">Address</label
-      ><br /><input
-        type="text"
-        id="Address"
-        class="border border-gray-300"
-        name="Address"
-        value=""
-        size="40"
-        maxlength="100"
-      /><br />
-      <label for="City" class="zemText zemRequired City">City</label
-      ><br /><input
-        type="text"
-        id="City"
-        class="zemText zemRequired"
-        name="City"
-        value=""
-        maxlength="100"
-      /><br />
-      <label
-        for="q5e178542b85fb18ca3c459e9a95f4f2e"
-        class="zemText zemRequired q5e178542b85fb18ca3c459e9a95f4f2e"
-        >Zip Code</label
-      ><br /><input
-        type="text"
-        id="q5e178542b85fb18ca3c459e9a95f4f2e"
-        class="zemText zemRequired"
-        name="q5e178542b85fb18ca3c459e9a95f4f2e"
-        value=""
-        maxlength="100"
-      /><br />
-      <label for="Phone" class="zemText zemRequired Phone">Phone</label
-      ><br /><input
-        type="text"
-        id="Phone"
-        class="zemText zemRequired"
-        name="Phone"
-        value=""
-        maxlength="100"
-      /><br />
-      <hr class="my-8"/>
-      <p class="mb-4 text-xl">
-  
-      Your Interests (check all that apply):
-      </p>
-      <input type="checkbox" id="Acting" class="zemCheckbox" name="Acting" />
-      <label for="Acting" class="zemCheckbox Acting">Acting</label><br />
-      <input
-        type="checkbox"
-        id="Production"
-        class="zemCheckbox"
-        name="Production"
-      />
-      <label for="Production" class="zemCheckbox Production">Production</label
-      ><br />
-      <input
-        type="checkbox"
-        id="Director"
-        class="zemCheckbox"
-        name="Director"
-      />
-      <label for="Director" class="zemCheckbox Director">Director</label><br />
-      <input
-        type="checkbox"
-        id="q7d25ea95e7e29f0e53fb5149cc383c1d"
-        class="zemCheckbox"
-        name="q7d25ea95e7e29f0e53fb5149cc383c1d"
-      />
-      <label
-        for="q7d25ea95e7e29f0e53fb5149cc383c1d"
-        class="zemCheckbox q7d25ea95e7e29f0e53fb5149cc383c1d"
-        >Stage Manager/Assistant Stage Manager</label
-      ><br />
-      <input
-        type="checkbox"
-        id="q85b5efd9aa41e61cb2fc4733a8c431fb"
-        class="zemCheckbox"
-        name="q85b5efd9aa41e61cb2fc4733a8c431fb"
-      />
-      <label
-        for="q85b5efd9aa41e61cb2fc4733a8c431fb"
-        class="zemCheckbox q85b5efd9aa41e61cb2fc4733a8c431fb"
-        >Light Board Operator</label
-      ><br />
-      <input
-        type="checkbox"
-        id="qd2668b10e909dd789973733ba03c5746"
-        class="zemCheckbox"
-        name="qd2668b10e909dd789973733ba03c5746"
-      />
-      <label
-        for="qd2668b10e909dd789973733ba03c5746"
-        class="zemCheckbox qd2668b10e909dd789973733ba03c5746"
-        >Sound Board Operator</label
-      ><br />
-      <input
-        type="checkbox"
-        id="qe81511041ebc711cdc6e9f9563d9a47d"
-        class="zemCheckbox"
-        name="qe81511041ebc711cdc6e9f9563d9a47d"
-      />
-      <label
-        for="qe81511041ebc711cdc6e9f9563d9a47d"
-        class="zemCheckbox qe81511041ebc711cdc6e9f9563d9a47d"
-        >Set Design</label
-      ><br />
-      <input
-        type="checkbox"
-        id="qd6ed0cb399e1635f8866c397e4ce4b55"
-        class="zemCheckbox"
-        name="qd6ed0cb399e1635f8866c397e4ce4b55"
-      />
-      <label
-        for="qd6ed0cb399e1635f8866c397e4ce4b55"
-        class="zemCheckbox qd6ed0cb399e1635f8866c397e4ce4b55"
-        >Set Construction/Strike, Painting</label
-      ><br />
-      <input type="checkbox" id="Props" class="zemCheckbox" name="Props" />
-      <label for="Props" class="zemCheckbox Props">Props</label><br />
-      <input
-        type="checkbox"
-        id="Wardrobe"
-        class="zemCheckbox"
-        name="Wardrobe"
-      />
-      <label for="Wardrobe" class="zemCheckbox Wardrobe">Wardrobe</label><br />
-      <input type="checkbox" id="Makeup" class="zemCheckbox" name="Makeup" />
-      <label for="Makeup" class="zemCheckbox Makeup">Makeup</label><br />
-      <input
-        type="checkbox"
-        id="Stagehand"
-        class="zemCheckbox"
-        name="Stagehand"
-      />
-      <label for="Stagehand" class="zemCheckbox Stagehand">Stagehand</label
-      ><br />
-      <input
-        type="checkbox"
-        id="q20165de7491435a41583d2c3686dd55c"
-        class="zemCheckbox"
-        name="q20165de7491435a41583d2c3686dd55c"
-      />
-      <label
-        for="q20165de7491435a41583d2c3686dd55c"
-        class="zemCheckbox q20165de7491435a41583d2c3686dd55c"
-        >Movie Projection</label
-      ><br />
-      <input
-        type="checkbox"
-        id="q49a368e9b39f9674178faed0956eb76e"
-        class="zemCheckbox"
-        name="q49a368e9b39f9674178faed0956eb76e"
-      />
-      <label
-        for="q49a368e9b39f9674178faed0956eb76e"
-        class="zemCheckbox q49a368e9b39f9674178faed0956eb76e"
-        >Ticket Window</label
-      ><br />
-      <input
-        type="checkbox"
-        id="qb921127f9ea37f6524eeb78541b46646"
-        class="zemCheckbox"
-        name="qb921127f9ea37f6524eeb78541b46646"
-      />
-      <label
-        for="qb921127f9ea37f6524eeb78541b46646"
-        class="zemCheckbox qb921127f9ea37f6524eeb78541b46646"
-        >Greeter, Concession Sales, Usher</label
-      ><br />
-      <input
-        type="checkbox"
-        id="qbf420776af6961ba792a744f979e25b0"
-        class="zemCheckbox"
-        name="qbf420776af6961ba792a744f979e25b0"
-      />
-      <label
-        for="qbf420776af6961ba792a744f979e25b0"
-        class="zemCheckbox qbf420776af6961ba792a744f979e25b0"
-        >Administrative Support</label
-      ><br />
-      <input
-        type="checkbox"
-        id="q0a3d0b04e427240e8e16f932c8c7f847"
-        class="zemCheckbox"
-        name="q0a3d0b04e427240e8e16f932c8c7f847"
-      />
-      <label
-        for="q0a3d0b04e427240e8e16f932c8c7f847"
-        class="zemCheckbox q0a3d0b04e427240e8e16f932c8c7f847"
-        >Facility Enhancement</label
-      ><br />
-      <hr class="my-8"/>
-      Availability (check all that apply):<br />
-      <input
-        type="checkbox"
-        id="Weekdays"
-        class="zemCheckbox"
-        name="Weekdays"
-      />
-      <label for="Weekdays" class="zemCheckbox Weekdays">Weekdays</label><br />
-      <input
-        type="checkbox"
-        id="Weekends"
-        class="zemCheckbox"
-        name="Weekends"
-      />
-      <label for="Weekends" class="zemCheckbox Weekends">Weekends</label><br />
-      <input
-        type="checkbox"
-        id="Mornings"
-        class="zemCheckbox"
-        name="Mornings"
-      />
-      <label for="Mornings" class="zemCheckbox Mornings">Mornings</label><br />
-      <input
-        type="checkbox"
-        id="Afternoons"
-        class="zemCheckbox"
-        name="Afternoons"
-      />
-      <label for="Afternoons" class="zemCheckbox Afternoons">Afternoons</label
-      ><br />
-      <input
-        type="checkbox"
-        id="Evenings"
-        class="zemCheckbox"
-        name="Evenings"
-      />
-      <label for="Evenings" class="zemCheckbox Evenings">Evenings</label><br />
+	<div class="mx-auto">
+		<div class="w-full mx-auto md:w-3/4 lg:w-9/12 xl:3/4 lg:text-xl xl:text-2xl">
+			<UiHeadline level="1">
+				Volunteer Form
+			</UiHeadline>
+			<article class="p-6 mt-6 bg-white rounded shadow lg:p-8">
+				<form name="volunteer-form" method="POST" data-netlify="true" action="success-contact/" netlify-honeypot="bot-field">
+					<p class="hidden">
+						<label>
+							Don’t fill out this field if you are human <input class="form-field" name="bot-field" />
+						</label>
+					</p>
+					<h2 class="mb-6 text-3xl font-semibold">
+						We Need You!
+					</h2>
+					<p class="max-w-3xl mb-6 text-xl">
+						As one of our volunteers, you’ll serve as a goodwill ambassador for the Old Theater and will have fun working on outstanding events and productions. No matter what your experience level, the Old Theater has opportunities for you to get involved! Tell us how you’d like to participate in a theater-sponsored event by filling out this form. 
+					</p>
 
-      <hr class="my-8"/>
-      
-      <label
-        for="q707ee00657f16ee7dda23657f89936a2"
-        class="zemTextarea q707ee00657f16ee7dda23657f89936a2"
-        >Comments, Other Info:</label
-      ><br /><textarea
-        id="q707ee00657f16ee7dda23657f89936a2"
-        class="zemTextarea"
-        name="q707ee00657f16ee7dda23657f89936a2"
-        cols="78"
-        rows="8"
-      ></textarea
-      ><br />
-      <br />
-      <input
-        type="submit"
-        class="zemSubmit"
-        name="zem_contact_submit"
-        value="Submit"
-      />
-    </div>
-  </form>
+					<div class="flex flex-wrap mb-6 -mx-3">
+						<div class="w-full px-3 mb-6 md:w-1/2 md:mb-0">
+							<label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="first-name">
+								First Name
+								<span class="ml-1 text-gray-600">
+									(required)
+								</span>
+							</label>
+							<input class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-800 rounded appearance-none focus:bg-white focus:border-gray-900 focus:outline-none" id="first-name" name="firstname" type="text" placeholder="Jane" required />
+						</div>
+						<div class="w-full px-3 md:w-1/2 md:mb-0">
+							<label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="lastname">
+								Last Name 
+								<span class="ml-1 text-gray-600">
+									(required)
+								</span>
+							</label>
+							<input class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-800 rounded appearance-none focus:bg-white focus:border-gray-900 focus:outline-none" id="grid-last-name" type="text" name="lastname" placeholder="Doe" required />
+						</div>
+					</div>
+          
+					<div class="w-full mb-8">
+						<div class="w-full mb-6 md:mb-0">
+							<label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="address">
+								address
+								<span class="ml-1 text-gray-600">
+									(required)
+								</span>
+							</label>
+							<input class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-800 rounded appearance-none focus:bg-white focus:border-gray-900 focus:outline-none" id="address" type="text" name="address" placeholder="me@mail.com" required />
+						</div>
+					</div>
+
+					<div class="flex flex-wrap mb-6 -mx-3">
+						<div class="w-full px-3 mb-6 md:w-1/2 md:mb-0">
+							<label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="email">
+								Email
+								<span class="ml-1 text-gray-600">
+									(required)
+								</span>
+							</label>
+							<input class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-800 rounded appearance-none focus:bg-white focus:border-gray-900 focus:outline-none" id="email" type="email" name="email" placeholder="me@mail.com" required />
+						</div>
+						<div class="w-full px-3 mb-3 md:w-1/2 md:mb-0">
+							<label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="phone">
+								Phone 
+							</label>
+							<input class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-800 rounded appearance-none focus:bg-white focus:border-gray-900 focus:outline-none" id="phone" name="phone" type="text" placeholder="252-555-4444" />
+						</div>
+					</div>
+
+          
+          <p class="mb-4 text-xl font-semibold">
+            Your Interests (check all that apply): 
+          </p>
+          <div class="text-lg">
+            <input type="checkbox" id="Acting" name="Acting" /> 
+            <label class="ml-2" for="Acting" >
+              Acting
+            </label>
+            <br /> <input type="checkbox" id="Production" name="Production" /> 
+            <label class="ml-2" for="Production" >
+              Production
+            </label>
+            <br /> <input type="checkbox" id="Director" name="Director" /> 
+            <label for="Director" >
+              Director
+            </label>
+            <br /> <input type="checkbox" id="stage-manager" name="stage-manager" /> 
+            <label for="stage-manager">
+              Stage Manager/Assistant Stage Manager
+            </label>
+            <br /> <input type="checkbox" id="light-board-operator" name="light-board-operator" /> 
+            <label for="light-board-operator">
+              Light Board Operator
+            </label>
+            <br /> <input type="checkbox" id="sound-board-operator" name="sound-board-operator" /> 
+            <label for="sound-board-operator">
+              Sound Board Operator
+            </label>
+            <br /> <input type="checkbox" id="set-design" name="set-design" /> 
+            <label for="set-design">
+              Set Design
+            </label>
+            <br /> <input type="checkbox" id="set-construction" name="set-construction" /> 
+            <label for="set-construction">
+              Set Construction/Strike, Painting
+            </label>
+            <br /> <input type="checkbox" id="Props" name="Props" /> 
+            <label for="Props" class="zemCheckbox Props">
+              Props
+            </label>
+            <br /> <input type="checkbox" id="Wardrobe" name="Wardrobe" /> 
+            <label for="Wardrobe" >
+              Wardrobe
+            </label>
+            <br /> <input type="checkbox" id="Makeup" name="Makeup" /> 
+            <label for="Makeup" >
+              Makeup
+            </label>
+            <br /> <input type="checkbox" id="Stagehand" name="Stagehand" /> 
+            <label for="Stagehand" >
+              Stagehand
+            </label>
+            <br /> <input type="checkbox" id="movie-projection" name="movie-projection" /> 
+            <label for="movie-projection" >
+              Movie Projection
+            </label>
+            <br /> <input type="checkbox" id="ticket-window" name="ticket-window" /> 
+            <label for="ticket-window" >
+              Ticket Window
+            </label>
+            <br /> <input type="checkbox" id="greeter-concession-usher" name="greeter-concession-usher" /> 
+            <label for="greeter-concession-usher" >
+              Greeter, Concession Sales, Usher
+            </label>
+            <br /> <input type="checkbox" id="admin-support" name="admin-support" /> 
+            <label for="admin-support" >
+              Administrative Support
+            </label>
+            <br /> <input type="checkbox" id="facility-enhancement" name="facility-enhancement" /> 
+            <label for="facility-enhancement" >
+              Facility Enhancement
+            </label>
+            
+          </div>
+            
+					<br /> <hr class="my-8" /> Availability (check all that apply):<br /> <input type="checkbox" id="Weekdays" name="Weekdays" /> 
+					<label for="Weekdays" class="zemCheckbox Weekdays">
+						Weekdays
+					</label>
+					<br /> <input type="checkbox" id="Weekends" name="Weekends" /> 
+					<label for="Weekends" >
+						Weekends
+					</label>
+					<br /> <input type="checkbox" id="Mornings" name="Mornings" /> 
+					<label for="Mornings" >
+						Mornings
+					</label>
+					<br /> <input type="checkbox" id="Afternoons" name="Afternoons" /> 
+					<label for="Afternoons" >
+						Afternoons
+					</label>
+					<br /> <input type="checkbox" id="Evenings" name="Evenings" /> 
+					<label for="Evenings" >
+						Evenings
+					</label>
+					<br /> <hr class="my-8" /> 
+					<label for="comments" >
+						Comments, Other Info:
+					</label>
+					<br />
+					<textarea id="comments"  name="comments" cols="78" rows="8"></textarea>					
+          <button class="px-4 py-2 font-bold text-white uppercase bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline" type="submit">Send</button>
+				</form>
+			</article>
+		</div>
+	</div>
 </template>
 
-<script>
-export default {
-  metaInfo: {
-    title: "Volunteer Registration Form",
-  },
 
-  layout: "NewLayout",
-};
+<script>
+  import UiHeadline from "@/components/ui/UiHeadline";
+  export default {
+    metaInfo: {
+      title: "Volunteer Registration Form",
+    },
+
+    layout: "NewLayout",
+    components: {
+      UiHeadline,
+    },
+  };
 </script>
