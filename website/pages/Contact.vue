2714f46027e0a8daea0c7c870f9ac74e8898aa78
Steven Loe <stevenjloe@protonmail.com>
2021-06-22 16:14:39 -0400

    fix: contact form

--- website/pages/Contact.vue
+++ website/pages/Contact.vue
@@ -31,24 +31,25 @@
 <p class="hidden"> <label >Don’t fill out this field if you are human <input class="form-field" name="bot-field"/></label></p>
 <div class="flex flex-wrap mb-6 -mx-3"><div class="w-full px-3 mb-6 md:mb-0"><label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="subject">Subject<span class="ml-1 text-gray-600">(required)</span></label>
 
-<select
-  class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-800 rounded focus:bg-white focus:border-gray-900 focus:outline-none" required >
-  <option name="subject" value="" >Please select a subject</option>
-  <option value="General">General</option>
-  <option value="Box Office">Box Office</option>
-  <option value="Facilities">Facilities</option>
-  <option value="Membership">Membership</option>
-  <option value="Rent the Theater">Rent the Theater</option>
-  <option value="Sponsor a Show">Sponsor a Show</option>
-  <option value="Treasurer">Treasurer</option>
-  <option value="Technical Services">Technical Services</option>
-  <option value="Membership / Volunteering">Membership / Volunteering</option>
-  <option value="Website">Website</option>
-  <option value="Other">Other</option>
+<select id="subject"
+class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-800 rounded focus:bg-white focus:border-gray-900 focus:outline-none" required >
+<option name="subject" value="" >Please select a subject</option>
+<option value="General">General</option>
+<option value="Box Office">Box Office</option>
+<option value="Facilities">Facilities</option>
+<option value="Membership">Membership</option>
+<option value="Rent the Theater">Rent the Theater</option>
+<option value="Sponsor a Show">Sponsor a Show</option>
+<option value="Treasurer">Treasurer</option>
+<option value="Technical Services">Technical Services</option>
+<option value="Membership / Volunteering">Membership / Volunteering</option>
+<option value="Website">Website</option>
+<option value="Other">Other</option>
 </select>
+
 </div></div>        
 <div class="flex flex-wrap mb-6 -mx-3"><div class="w-full px-3 mb-6 md:w-1/2 md:mb-0"><label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="first-name">First Name<span class="ml-1 text-gray-600">(required)</span></label><input class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-800 rounded appearance-none focus:bg-white focus:border-gray-900 focus:outline-none" id="first-name" name="firstname" type="text" placeholder="Jane" required /></div>
-<div class="w-full px-3 mb-3 md:w-1/2 md:mb-0"><label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="lastname">Last Name <span class="ml-1 text-gray-600">(required)</span></label><input class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-800 rounded appearance-none focus:bg-white focus:border-gray-900 focus:outline-none" id="grid-last-name"type="text" name="lastname" placeholder="Doe" required /></div></div>
+<div class="w-full px-3 mb-3 md:w-1/2 md:mb-0"><label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="lastname">Last Name <span class="ml-1 text-gray-600">(required)</span></label><input id="lastname" class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-800 rounded appearance-none focus:bg-white focus:border-gray-900 focus:outline-none" type="text" name="lastname" placeholder="Doe" required /></div></div>
 <div class="flex flex-wrap mb-6 -mx-3"><div class="w-full px-3 mb-6 md:w-1/2 md:mb-0"><label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="email">Email<span class="ml-1 text-gray-600">(required)</span></label><input class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-800 rounded appearance-none focus:bg-white focus:border-gray-900 focus:outline-none" id="email" type="email" name="email" placeholder="me@mail.com" required /></div>
 <div class="w-full px-3 mb-3 md:w-1/2 md:mb-0"> <label class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" for="phone">Phone </label> <input class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-800 rounded appearance-none focus:bg-white focus:border-gray-900 focus:outline-none" id="phone" name="phone" type="text" placeholder="252-555-4444"/></div>
 </div>
