428e37df16cc68fc5fc899561f2935792b210c90
Steven Loe <stevenjloe@protonmail.com>
2021-06-29 15:57:12 -0400

    refactor: shows page should use list components

--- website/pages/shows/index.vue
+++ website/pages/shows/index.vue
@@ -1,148 +1,43 @@
 <template>
-  <div>
-    <article class="w-full max-w-4xl mx-auto lg:text-xl xl:text-2xl">
-      <UiHeadline class="mb-6" level="1">Upcoming Shows</UiHeadline>
-
-      <article
-        class="p-8 pb-12 mb-8 bg-white rounded-lg shadow-lg"
-        v-for="show of futureShows"
-        :key="show.title"
-      >
-
-        <div
-          class="mb-1 text-sm font-semibold text-gray-700 uppercase md:text-base lg:text-lg"
-        >
-          {{ showTime(show.eventTime) }}
-        </div>
-
-        <h2
-          class="mb-4 text-xl font-semibold sm:text-2xl md:text-3xl lg:text-4xl"
-        >
-          <nuxt-link :to="`${show.slug}?page=index`">
-            {{ show.title }}</nuxt-link
-          >
-        </h2>
-
-        <nuxt-content
-          class="mx-auto prose-sm prose sm:prose lg:prose-lg xl:prose-2xl"
-          :document="show"
-        />
-      </article>
-
-
-      <!-- </div> -->
+  <div class="mx-auto ">
+    <article class="container mx-auto">
+      <UiHeadline class="pl-4 mb-6" level="1">Upcoming Shows</UiHeadline>
+      <ShowList :shows="futureShows" />
     </article>
 
-    
-    <article class="w-full max-w-4xl mx-auto lg:text-xl xl:text-2xl">
+    <article class="container p-4 mx-auto md:p-8 lg:text-xl xl:text-2xl">
       <UiHeadline class="mb-6" level="1">Past Shows</UiHeadline>
-      <article
-        class="p-8 pb-12 mb-8 bg-white rounded-lg shadow-lg"
-        v-for="show of pastShows"
-        :key="show.title"
-      >
-      <div
-          class="mb-1 text-sm font-semibold text-gray-700 uppercase md:text-base lg:text-lg"
-        >
-         PAST SHOW
-        </div>
-      
-
-        <div
-          class="mb-1 text-sm font-semibold text-gray-700 uppercase md:text-base lg:text-lg"
-        >
-          {{ showTime(show.eventTime) }}
-        </div>
-
-        <h2
-          class="mb-4 text-xl font-semibold text-gray-500 sm:text-2xl md:text-3xl lg:text-4xl"
-        >
-          <nuxt-link :to="`${show.slug}?page=index`">
-            {{ show.title }}</nuxt-link
-          >
-        </h2>
-
-        <nuxt-content
-          class="mx-auto prose-sm prose sm:prose lg:prose-lg xl:prose-2xl"
-          :document="show"
-        />
-      </article>
-
-
+      <PastShowList :pastShows="pastShows" />
     </article>
   </div>
 </template>
 
-
-
-
 <script>
-import UiHeadline from "@/components/ui/UiHeadline";
-import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
+import UiHeadline from "@/components/ui/UiHeadline"
+import ShowList from "@/components/ShowList"
+import PastShowList from "@/components/PastShowList"
 
-const localizedFormat = require("dayjs/plugin/localizedFormat");
-dayjs.extend(localizedFormat);
-dayjs.extend(utc);
+import {sortShows} from '@/utils/sort.js'
 
 export default {
   data() {
     return {
-      shows: [],
       futureShows: [],
-      pastShows: []
-    }
+      pastShows: [],
+    };
   },
   async asyncData({ $content }) {
     const shows = await $content("shows")
-      .sortBy("eventTime", "desc")
-      .limit(10)
+      .sortBy("date", "desc")
       .fetch();
 
-      shows.sort(function(a, b) {
-        var c = new Date(a.eventDate);
-        var d = new Date(b.eventDate);
-        return d-c;
-      });
-
-      const today = dayjs.utc().startOf('day')
-      console.log( "today", today.format('dddd, MMMM D h:mm A'));
-
-      const pastShows = []
-      const futureShows = [] 
-
-      shows.forEach(event => {
-      // console.log(event);
-      let EDate = dayjs.utc((event.eventDate))
-      console.log("eDate", event.title, "date", EDate.format('dddd, MMMM D h:mm A'));
-      if (today.isAfter(EDate)) {
-        console.log("past event", event.title);
-        pastShows.push(event)
-        console.log("Added to Past SHows", pastShows);
-      } else {
-        console.log("future event", event.title);
-        futureShows.push(event)
-      }
-    })
-
-    // console.log("FS", futureShows);
-    // console.log( "PS", pastShows);
-
-
-
-    return {  shows, futureShows, pastShows };
-  },
-  methods: {
-    showTime(date) {
-      return dayjs(date).format("LL");
-    },
-    listData() {
-      console.log("listData", this.shows);
-    }
+    return sortShows(shows)
   },
   layout: "NewLayout",
   components: {
-    UiHeadline
-  }
+    UiHeadline,
+    ShowList,
+    PastShowList
+  },
 };
 </script>
