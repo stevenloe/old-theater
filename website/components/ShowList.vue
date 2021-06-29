6f6814a20ce9f32042d2fe050df8e46a6b18a6e2
Steven Loe <stevenjloe@protonmail.com>
2021-06-29 15:20:12 -0400

    refactor: future shows to a component

---
+++ website/components/ShowList.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <div
+      class="p-0 m-4 rounded-lg shadow-lg sm:m-8 md:flex md:m-8 md:mr-8" style="border: solid 1px #999"
+      v-for="show of shows"
+      :key="show.title"
+    >
+      <!-- image card -->
+      <div
+        class="relative overflow-hidden rounded-lg md:w-7/12 md:rounded-r-none text"
+      >
+        <nuxt-link  :to="show.path">
+          <img class="object-cover w-full h-full " :src="`/images/shows/${show.img}`" :alt="show.imgAlt" />
+        </nuxt-link>
+      </div>
+
+      <!-- details card -->
+      <div
+        class="relative pb-3 -mt-1 bg-white border rounded-b-lg md:mt-0 md:rounded-b-none md:rounded-r-lg md:w-5/12"
+      >
+        <div class="w-full p-6 pt-2 pb-0">
+          <h3 class="mt-3 mb-2 text-4xl font-bold leading-none">
+            {{ show.title }}
+          </h3>
+          <div class="flex">
+            <ShowPill :showtype="show.showtype"></ShowPill>
+          </div>
+          <div
+            class="mb-4 text-sm font-semibold leading-relaxed tracking-wide uppercase flex-start"
+          >
+          <div>
+            
+          <span class="mr-2">{{ showDate(show.date) }}</span>
+        {{ showTime(show.date) }}
+          </div>
+            <span class="mr-2">${{ show.formattedPrice }}</span>
+            <div class="flex flex-wrap" v-if="show.location">
+              <div>At {{ show.location }}</div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="visible mx-6 my-6 text-gray-700 md:hidden lg:block xl:text-lg lg:mt-6"
+        >
+          {{ show.shortDesc }}
+        </div>
+
+        <div class="flex justify-end w-full pb-0 pl-6 pr-6 md:inline-block">
+          <a
+            v-if="show.ticketURL"
+            :href="show.ticketURL"
+            class="px-4 py-2 mb-3 mr-2 font-bold tracking-wide text-center text-white uppercase bg-blue-600 rounded-lg shadow-lg md:block hover:bg-blue-700"
+          >
+            Buy tickets</a
+          >
+          <nuxt-link
+            :to="show.path"
+            class="w-full px-4 py-2 mb-3 font-bold tracking-wide text-center text-white uppercase bg-gray-600 rounded-lg shadow-lg md:block hover:bg-gray-700"
+          >
+            Learn More</nuxt-link
+          >
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+
+import ShowPill from './ShowPill'
+import {formatShowDate} from '@/utils/dates.js'
+export default {
+  props: {
+    shows: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    showDate(date) {
+      return formatShowDate(date, "date")
+    },
+    showTime(date) {
+      return formatShowDate(date, "time")
+    }
+  },
+  components: {
+    ShowPill
+  }
+}
+</script>
--- website/pages/index.vue
+++ website/pages/index.vue
@@ -1,71 +1,9 @@
 <template>
   <div class="mx-auto">
     <div class="container mx-auto">
+      
       <AlertBox posts="alerts"/>
-      <div
-        class="p-0 m-4 rounded shadow-lg show-card sm:m-8 md:flex md:m-8 md:mr-8"
-        v-for="show of shows"
-        :key="show.title"
-      >
-        <!-- image card -->
-        <div
-          class="relative overflow-hidden rounded-lg md:w-7/12 md:rounded-r-none text"
-        >
-          <nuxt-link :to="show.path">
-            <img
-              :src="require(`~/assets/img_new/processed/${show.img}`)"
-              :alt="show.imgAlt"
-            />
-          </nuxt-link>
-        </div>
-
-        <!-- details card -->
-        <div
-          class="relative pb-3 -mt-1 bg-white rounded-b-lg md:mt-0 md:rounded-b-none md:rounded-r-lg md:w-5/12"
-        >
-          <div class="w-full p-6 pt-2 pb-0">
-            <h3 class="mt-3 mb-2 text-4xl font-bold leading-none">
-              {{ show.title }}
-            </h3>
-            <div class="flex">
-              <ShowPill :showtype="show.showtype"></ShowPill>
-            </div>
-            <div
-              class="flex flex-wrap mb-4 text-sm font-semibold leading-relaxed tracking-wide uppercase flex-start"
-            >
-              <span class="mr-2">{{ show.showDate }}</span>
-        
-              <span class="mr-2">{{ show.showTime }} </span>
-              <span class="mr-2">${{ show.formattedPrice }}</span>
-
-              <div class="flex flex-wrap" v-if="show.location">
-                <div>At {{ show.location }}</div>
-              </div>
-              
-              
-            </div>
-          </div>
-          
-          <div class="visible mx-6 my-6 text-gray-700 md:hidden lg:block xl:text-lg lg:mt-6">{{show.shortDesc}} </div>
-
-          <div class="flex justify-end w-full pb-0 pl-6 pr-6 md:inline-block">
-            <a
-              v-if="show.ticketURL"
-              :href="show.ticketURL"
-              class="px-4 py-2 mb-3 mr-2 font-bold tracking-wide text-center text-white uppercase bg-blue-600 rounded-lg shadow-lg md:block hover:bg-blue-700"
-            >
-              Buy tickets</a
-            >
-            <nuxt-link
-              :to="show.path"
-              class="w-full px-4 py-2 mb-3 font-bold tracking-wide text-center text-white uppercase bg-gray-600 rounded-lg shadow-lg md:block hover:bg-gray-700"
-            >
-              Learn More</nuxt-link
-            >
-          </div>
-        </div>
-      </div>
-
+      <ShowList :shows="futureShows" />
       <NewsWidget  :news="news"/>
       <CommunityPartners />
       <Membership />
@@ -81,6 +19,7 @@
 </template>
 
 <script>
+import ShowList from "@/components/ShowList";
 import AlertBox from "@/components/AlertBox";
 import CommunityPartners from "@/components/CommunityPartners";
 import Membership from "@/components/Membership";
@@ -88,6 +27,8 @@ import MemberCarousel from "@/components/MemberCarousel";
 import Sponsorship from "@/components/Sponsorship";
 import NewsWidget from "@/components/NewsWidget";
 import ShowPill from "@/components/ShowPill";
+
+import {sortShows} from '@/utils/sort.js'
 
 export default {
   head() {
@@ -97,23 +38,32 @@ export default {
       ],
     };
   },
+    data() {
+    return {
+      futureShows: [],
+      pastShows: [],
+    };
+  },
 
   async asyncData ({ $content, params }) {
   const [shows, news, alerts] = await Promise.all([ 
-    $content("shows", params.slug).sortBy("eventTime", "asc").fetch(),
+    $content("shows", params.slug).sortBy("date", "asc").fetch(),
     $content("news", params.slug).sortBy("eventTime", "desc").fetch(),
     $content("alerts", params.slug).fetch(),
-    
   ])
+
+  const {futureShows, pastShows} = sortShows(shows)
 
   return {
     shows: shows,
+    futureShows:futureShows,
     news: news,
     alerts: alerts
   }
 },
   layout: "NewLayout",
   components: {
+    ShowList,
     AlertBox,
     CommunityPartners,
     Membership,
@@ -125,11 +75,6 @@ export default {
 };
 </script>
 
-<style>
-.show-card {
-  /* box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.2); */
-}
-</style>
 
 
 
