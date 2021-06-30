8e3dc2a0c559eea7491ab842f08f23a9a6c366de
Steven Loe <stevenjloe@protonmail.com>
2021-06-30 18:30:01 -0400

    fix: nav bar items should not wrap

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -12,8 +12,20 @@ module.exports = {
       'nuxt.config.ts'
     ]
   },
-
   theme: {
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '834px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
     extend: {
       typography: {
         DEFAULT: {
