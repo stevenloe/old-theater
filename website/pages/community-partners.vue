dd9caf95d4730ea36a4ddd556bb7e1ce9a1bcd90
Steven Loe <stevenjloe@protonmail.com>
2021-06-30 17:09:56 -0400

    content: update community partners page text, logos

--- website/pages/community-partners.vue
+++ website/pages/community-partners.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="mx-auto">
-    <div
-      class="w-full mx-auto content-column lg:text-xl xl:text-2xl"
-    >
+    <div class="w-full mx-auto content-column lg:text-xl xl:text-2xl">
       <div class="mb-8">
-        <UiHeadline level="1"
-          >Community Partners</UiHeadline
-        >
+        <UiHeadline level="1">Community Partners</UiHeadline>
         <h1 class="mt-4 mb-4 text-4xl font-extrabold"></h1>
 
-        <div class="p-8 mb-8 bg-white rounded-lg shadow-lg">
+        <div class="p-8 mb-8 bg-white rounded-lg shadow-lg content-border">
           <div>
             <img
               src="https://old-theater-pics.netlify.app/img/ctw-2019.jpg"
@@ -21,24 +17,36 @@
               Theater.
             </p>
           </div>
-          <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
+          <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
             The Old Theater's goal is to enrich, educate and entertain our
             community by serving as a great venue for the performing arts. We
-            partner with local arts organizations to help them bring educational, live theater,  &amp; live music performances to our
+            partner with local arts organizations to help them bring
+            educational, live theater, &amp; live music performances to our
             stage.
           </p>
-          <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
+          <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
             Below are some of the organizations we partner with.
           </p>
         </div>
 
-        <div class="p-8 mb-8 bg-orange-200 rounded-lg shadow-lg">
-          <UiHeadline class="mb-4" level="3"
-            >Pamlico Musical Society</UiHeadline
-          >
-          <UiHeadline class="mb-4" level="5"
-          >Remarkable Musical Experiences for our Amazing Community</UiHeadline
-        >
+        <div class="p-8 mb-8 bg-orange-200 rounded-lg shadow-lg content-border">
+          <div class="flex justify-between">
+            <div class="w-4/5">
+              <UiHeadline class="mb-2" level="3"
+                >Pamlico Musical Society</UiHeadline
+              >
+              <UiHeadline class="mb-4" level="4"
+                >Remarkable Musical Experiences for our Amazing
+                Community</UiHeadline
+              >
+            </div>
+
+            <img
+              class="object-contain h-16 ml-2 md:h-20"
+              src="/images/community/pms_logo.jpg"
+              alt="Pamlico Musical Society logo"
+            />
+          </div>
           <div class="mb-8">
             <img
               src="https://old-theater-pics.netlify.app/img/pms.png"
@@ -46,14 +54,30 @@
               alt="Pamlico musical society"
             />
           </div>
-          <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
-            The mission of the Pamlico Musical Society is to present diverse musical experiences that enrich the quality of life in our community.  Since 1997, our concert performances and outreach activities have provided opportunities to enjoy and interact with a wide variety of high quality musical groups representing many musical genres.
+          <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
+            The mission of the Pamlico Musical Society is to present diverse
+            musical experiences that enrich the quality of life in our
+            community. Since 1997, our concert performances and outreach
+            activities have provided opportunities to enjoy and interact with a
+            wide variety of high quality musical groups representing many
+            musical genres.
           </p>
-          <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
-            Our primary performance venue is the Old Theater, where the small hall promotes intimate exchanges between the artists and audiences &mdash; further enhancing the significance of the musical experience.  Many of our artists also present an outreach component which extends the impact of our programs to partners including the Pamlico County Public Schools, the Arapahoe Charter School, the Pamlico Christian Academy, HeartWorks, Prime Time, the Holt's Chapel Community Center, and the Pamlico Community College.
+          <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
+            Our primary performance venue is the Old Theater, where the small
+            hall promotes intimate exchanges between the artists and audiences
+            &mdash; further enhancing the significance of the musical
+            experience. Many of our artists also present an outreach component
+            which extends the impact of our programs to partners including the
+            Pamlico County Public Schools, the Arapahoe Charter School, the
+            Pamlico Christian Academy, HeartWorks, Prime Time, the Holt's Chapel
+            Community Center, and the Pamlico Community College.
           </p>
-          <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
-            With a season that runs from September/October through April/May each year, our Grammy-quality programming embraces bluegrass, jazz, classical, folk, rock, Americana, pop, blues and more, offering something special for everyone who loves great music.To learn more visit
+          <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
+            With a season that runs from September/October through April/May
+            each year, our Grammy-quality programming embraces bluegrass, jazz,
+            classical, folk, rock, Americana, pop, blues and more, offering
+            something special for everyone who loves great music.To learn more
+            visit
             <a class="underline" href="https://www.pamlicomusic.org"
               >www.pamlicomusic.org</a
             >.
@@ -61,7 +85,7 @@
         </div>
       </div>
 
-      <div class="p-8 mb-8 bg-pink-100 rounded-lg shadow-lg">
+      <div class="p-8 mb-8 bg-pink-100 rounded-lg shadow-lg content-border">
         <div class="flex justify-between">
           <div class="w-4/5">
             <UiHeadline class="mb-2" level="3"
@@ -69,11 +93,14 @@
             >
             <UiHeadline class="mb-4" level="4"
               >A Place for Kids to Learn, Grow and Imagine.</UiHeadline
-            >  
+            >
           </div>
-         
-            <img class="object-contain h-16" src="/images/community/ctw-logo.gif" alt="Childrens Theater Workshop logo">
-        
+
+          <img
+            class="object-contain h-20 ml-1"
+            src="/images/community/ctw-logo.gif"
+            alt="Childrens Theater Workshop logo"
+          />
         </div>
         <div class="mb-8">
           <img
@@ -83,13 +110,13 @@
           />
         </div>
 
-        <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
+        <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
           Children's Theater Workshop has a longstanding commitment to enriching
           the lives of children by providing a structured arts program led by an
           exceptional, and carefully chosen, theater arts Drama Coach.
         </p>
 
-        <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
+        <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
           Besides being great fun, children's theater has many benefits.
           Activities like improv and drama performance can boost self-esteem and
           confidence. The theater is a place where inner silliness and
@@ -97,18 +124,24 @@
           themselves, it translates to other aspects of their lives &mdash;
           school, communication, and social situations.
         </p>
-        <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
-          During the age-divided, two-week sessions, children are introduced to theater skills such as acting fundamentals, learning to speak on stage, using drama exercises by playing theater games, see the behind-the-scenes work, learn to work in peer groups, and meet new friends.
+        <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
+          During the age-divided, two-week sessions, children are introduced to
+          theater skills such as acting fundamentals, learning to speak on
+          stage, using drama exercises by playing theater games, see the
+          behind-the-scenes work, learn to work in peer groups, and meet new
+          friends.
         </p>
-        <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
-Learning about the performing arts can teach important skills in life, such as self-confidence, cooperation, self-discipline, and concentration. Imagination is encouraged! More at
+        <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
+          Learning about the performing arts can teach important skills in life,
+          such as self-confidence, cooperation, self-discipline, and
+          concentration. Imagination is encouraged! More at
           <a class="underline" href="https://www.childrenstheateronc.com/"
             >www.childrenstheateronc.com</a
           >
         </p>
       </div>
 
-      <div class="p-8 pt-4 mb-8 bg-indigo-300 rounded-lg shadow-lg">
+      <div class="p-8 pt-4 mb-8 bg-indigo-100 rounded-lg shadow-lg content-border">
         <UiHeadline class="mb-4" level="3">Pamlico Community Band</UiHeadline>
         <div>
           <img
@@ -120,13 +153,13 @@ Learning about the performing arts can teach important skills in life, such as s
             <!-- Pamlico Community Band -->
           </p>
         </div>
-        <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
+        <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
           The Pamlico Community Band has members from Pamlico County and
           surrounding counties. Ages range from the young, to the young at
           heart! It doesn't matter what experience you have. Only thing that
           matters, is that you have a passion for music!
         </p>
-        <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
+        <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
           More at their
           <a
             class="underline"
@@ -138,7 +171,7 @@ Learning about the performing arts can teach important skills in life, such as s
         <div class="flex"></div>
       </div>
 
-      <div class="p-8 pt-4 mb-8 bg-gray-100 rounded-lg shadow-lg">
+      <div class="p-8 pt-4 mb-8 bg-gray-100 rounded-lg shadow-lg content-border">
         <UiHeadline class="mb-4" level="3">Pamlico Chorale</UiHeadline>
 
         <div class="float-right ml-2">
@@ -151,13 +184,13 @@ Learning about the performing arts can teach important skills in life, such as s
             <!-- The Pamlico Chorale  -->
           </p>
         </div>
-        <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
+        <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
           The Pamlico Chorale is an established choral group in Pamlico County,
           North Carolina. Its members enjoy singing and rejoicing together. Reba
           Tiller is the group's beloved, long-standing leader. Participation is
           open to all who share a love of music.
         </p>
-        <p class="mb-4 leading-7 text-gray-900 lg:leading-9 lg:mb-6">
+        <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
           More at their
           <a
             class="underline"
@@ -190,15 +223,10 @@ export default {
   layout: "NewLayout",
 
   async fetch() {
-
-
-
     this.members = await fetch(
       "https://old-theater.netlify.app/data/members2.json"
     ).then((res) => res.json());
   },
-
-
 };
 </script>
 
