794df51ae1d4293a03b4497a67d52c3a16dd3576
Steven Loe <stevenjloe@protonmail.com>
2021-07-02 11:04:23 -0400

    fix: show short should unshift past shows

--- website/utils/sort.js
+++ website/utils/sort.js
@@ -1,5 +1,3 @@
-
-
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 
@@ -12,34 +10,23 @@ export function sortShows(shows) {
   shows.sort(function (a, b) {
     var c = new Date(a.date);
     var d = new Date(b.date);
-    return d - c;
+    return c - d;
   });
 
   const today = dayjs.utc().startOf("day");
 
-
   shows.forEach((event) => {
-    // console.log(event);
     let EDate = dayjs.utc(event.date);
-    // console.log(
-    //   "eDate",
-    //   event.title,
-    //   "date",
-    //   EDate.format("dddd, MMMM D h:mm A")
-    // );
+
     if (today.isAfter(EDate)) {
-      // console.log("past event", event.title);
-      pastShows.push(event);
-      // console.log("Added to Past SHows", pastShows);
+      pastShows.unshift(event);
     } else {
-      // console.log("future event", event.title);
       futureShows.push(event);
     }
   });
 
   return { futureShows, pastShows };
-
-  }
+}
 
 
 
