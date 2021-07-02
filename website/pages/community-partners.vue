b81ffbe3e4d22b8c3f4699d2b54d57efcd7e40d5
Steven Loe <stevenjloe@protonmail.com>
2021-07-02 10:20:13 -0400

    fix: layout bugs community partners page

--- website/pages/community-partners.vue
+++ website/pages/community-partners.vue
@@ -1,209 +1,209 @@
 <template>
-  <div>
-    <div class="w-full max-w-4xl mx-auto lg:text-xl xl:text-2xl">
-      <UiHeadline level="1" class="mb-6">Community Partners</UiHeadline>
-      <div class="p-8 bg-white rounded shadow-lg content-border">
-        <div class="mb-4">
-          <div>
-            <img
-              src="https://old-theater-pics.netlify.app/img/ctw-2019.jpg"
-              class="w-full overflow-hidden rounded-lg shadow-lg content-border"
-              alt=""
-            />
-            <p class="p-1 mb-3 text-sm italic text-gray-700">
-              Children's Theater Workshop participants having fun at The Old
-              Theater.
-            </p>
-          </div>
-          <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
-            The Old Theater's goal is to enrich, educate and entertain our
-            community by serving as a great venue for the performing arts. We
-            partner with local arts organizations to help them bring
-            educational, live theater, &amp; live music performances to our
-            stage.
-          </p>
-          <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
-            Below are some of the organizations we partner with.
-          </p>
-        </div>
-
-        <div class="p-8 mb-8 bg-orange-200 rounded-lg shadow-lg content-border">
-          <div class="flex justify-between">
-            <div class="w-4/5">
-              <UiHeadline class="mb-2" level="3"
-                >Pamlico Musical Society</UiHeadline
-              >
-              <UiHeadline class="mb-4" level="4"
-                >Remarkable Musical Experiences for our Amazing
-                Community</UiHeadline
-              >
-            </div>
-
-            <img
-              class="object-contain h-16 ml-2 md:h-20"
-              src="/images/community/pms_logo.jpg"
-              alt="Pamlico Musical Society logo"
-            />
-          </div>
-          <div class="mb-8">
-            <img
-              src="https://old-theater-pics.netlify.app/img/pms.png"
-              class="w-full overflow-hidden rounded-lg shadow-lg"
-              alt="Pamlico musical society"
-            />
-          </div>
-          <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
-            The mission of the Pamlico Musical Society is to present diverse
-            musical experiences that enrich the quality of life in our
-            community. Since 1997, our concert performances and outreach
-            activities have provided opportunities to enjoy and interact with a
-            wide variety of high quality musical groups representing many
-            musical genres.
-          </p>
-          <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
-            Our primary performance venue is the Old Theater, where the small
-            hall promotes intimate exchanges between the artists and audiences
-            &mdash; further enhancing the significance of the musical
-            experience. Many of our artists also present an outreach component
-            which extends the impact of our programs to partners including the
-            Pamlico County Public Schools, the Arapahoe Charter School, the
-            Pamlico Christian Academy, HeartWorks, Prime Time, the Holt's Chapel
-            Community Center, and the Pamlico Community College.
-          </p>
-          <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
-            With a season that runs from September/October through April/May
-            each year, our Grammy-quality programming embraces bluegrass, jazz,
-            classical, folk, rock, Americana, pop, blues and more, offering
-            something special for everyone who loves great music.To learn more
-            visit
-            <a class="underline" href="https://www.pamlicomusic.org"
-              >www.pamlicomusic.org</a
-            >.
-          </p>
-        </div>
-      </div>
-
-      <div class="p-8 mb-8 bg-pink-100 rounded-lg shadow-lg content-border">
-        <div class="flex justify-between">
-          <div class="w-4/5">
-            <UiHeadline class="mb-2" level="3"
-              >Children's Theater Workshop</UiHeadline
-            >
-            <UiHeadline class="mb-4" level="4"
-              >A Place for Kids to Learn, Grow and Imagine.</UiHeadline
-            >
-          </div>
-
-          <img
-            class="object-contain h-20 ml-1"
-            src="/images/community/ctw-logo.gif"
-            alt="Childrens Theater Workshop logo"
-          />
-        </div>
-        <div class="mb-8">
-          <img
-            src="https://old-theater-pics.netlify.app/img/ctw.png"
-            class="w-full overflow-hidden rounded-lg shadow-lg"
-            alt=""
-          />
-        </div>
-
-        <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
-          Children's Theater Workshop has a longstanding commitment to enriching
-          the lives of children by providing a structured arts program led by an
-          exceptional, and carefully chosen, theater arts Drama Coach.
-        </p>
-
-        <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
-          Besides being great fun, children's theater has many benefits.
-          Activities like improv and drama performance can boost self-esteem and
-          confidence. The theater is a place where inner silliness and
-          creativity are embraced. When children learn to be more confident in
-          themselves, it translates to other aspects of their lives &mdash;
-          school, communication, and social situations.
-        </p>
-        <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
-          During the age-divided, two-week sessions, children are introduced to
-          theater skills such as acting fundamentals, learning to speak on
-          stage, using drama exercises by playing theater games, see the
-          behind-the-scenes work, learn to work in peer groups, and meet new
-          friends.
-        </p>
-        <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
-          Learning about the performing arts can teach important skills in life,
-          such as self-confidence, cooperation, self-discipline, and
-          concentration. Imagination is encouraged! More at
-          <a class="underline" href="https://www.childrenstheateronc.com/"
-            >www.childrenstheateronc.com</a
-          >
-        </p>
-      </div>
-
-      <div class="p-8 pt-4 mb-8 bg-indigo-100 rounded-lg shadow-lg content-border">
-        <UiHeadline class="mb-4" level="3">Pamlico Community Band</UiHeadline>
+  <div class="w-full max-w-4xl mx-auto lg:text-xl xl:text-2xl">
+    <UiHeadline level="1" class="mb-6">Community Partners</UiHeadline>
+    <div class="p-8 mb-8 bg-white rounded-lg shadow-lg content-border">
+      <div class="mb-4">
         <div>
           <img
-            src="https://old-theater-pics.netlify.app/img/community-band.jpg"
-            class="w-full overflow-hidden rounded-lg shadow-lg"
+            src="https://old-theater-pics.netlify.app/img/ctw-2019.jpg"
+            class="w-full overflow-hidden rounded-lg shadow-lg content-border"
             alt=""
           />
-          <p class="p-1 mb-3 text-xs italic text-blue">
-            <!-- Pamlico Community Band -->
+          <p class="p-1 mb-3 text-sm italic text-gray-700">
+            Children's Theater Workshop participants having fun at The Old
+            Theater.
           </p>
         </div>
-        <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
-          The Pamlico Community Band has members from Pamlico County and
-          surrounding counties. Ages range from the young, to the young at
-          heart! It doesn't matter what experience you have. Only thing that
-          matters, is that you have a passion for music!
+        <p
+          class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6"
+        >
+          The Old Theater's goal is to enrich, educate and entertain our
+          community by serving as a great venue for the performing arts. We
+          partner with local arts organizations to help them bring educational,
+          live theater, &amp; live music performances to our stage.
         </p>
-        <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
-          More at their
-          <a
-            class="underline"
-            href="https://www.facebook.com/Pamlico-Community-Band-138279799519612/"
-            >Facebook page</a
-          >
-        </p>
-
-        <div class="flex"></div>
-      </div>
-
-      <div class="p-8 pt-4 mb-8 bg-gray-100 rounded-lg shadow-lg content-border">
-        <UiHeadline class="mb-4" level="3">Pamlico Chorale</UiHeadline>
-
-        <div class="float-right ml-2">
-          <img
-            src="https://old-theater-pics.netlify.app/img/chorale.jpg"
-            class="w-full overflow-hidden rounded-lg"
-            alt=""
-          />
-          <p class="p-1 mb-3 text-xs italic text-blue">
-            <!-- The Pamlico Chorale  -->
-          </p>
-        </div>
-        <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
-          The Pamlico Chorale is an established choral group in Pamlico County,
-          North Carolina. Its members enjoy singing and rejoicing together. Reba
-          Tiller is the group's beloved, long-standing leader. Participation is
-          open to all who share a love of music.
-        </p>
-        <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
-          More at their
-          <a
-            class="underline"
-            href="https://www.facebook.com/Pamlico-Chorale-158261794372262/"
-          >
-            Facebook page</a
-          >
+        <p
+          class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6"
+        >
+          Below are some of the organizations we partner with.
         </p>
       </div>
-      <client-only>
-        <member-carousel />
-      </client-only>
     </div>
+
+    <div class="p-8 mb-8 bg-orange-200 rounded-lg shadow-lg content-border">
+      <div class="flex justify-between">
+        <div class="w-4/5">
+          <UiHeadline class="mb-2" level="3"
+            >Pamlico Musical Society</UiHeadline
+          >
+          <UiHeadline class="mb-4" level="4"
+            >Remarkable Musical Experiences for our Amazing
+            Community</UiHeadline
+          >
+        </div>
+
+        <img
+          class="object-contain h-16 ml-2 md:h-20"
+          src="/images/community/pms_logo.jpg"
+          alt="Pamlico Musical Society logo"
+        />
+      </div>
+      <div class="mb-8">
+        <img
+          src="https://old-theater-pics.netlify.app/img/pms.png"
+          class="w-full overflow-hidden rounded-lg shadow-lg"
+          alt="Pamlico musical society"
+        />
+      </div>
+      <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
+        The mission of the Pamlico Musical Society is to present diverse musical
+        experiences that enrich the quality of life in our community. Since
+        1997, our concert performances and outreach activities have provided
+        opportunities to enjoy and interact with a wide variety of high quality
+        musical groups representing many musical genres.
+      </p>
+      <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
+        Our primary performance venue is the Old Theater, where the small hall
+        promotes intimate exchanges between the artists and audiences &mdash;
+        further enhancing the significance of the musical experience. Many of
+        our artists also present an outreach component which extends the impact
+        of our programs to partners including the Pamlico County Public Schools,
+        the Arapahoe Charter School, the Pamlico Christian Academy, HeartWorks,
+        Prime Time, the Holt's Chapel Community Center, and the Pamlico
+        Community College.
+      </p>
+      <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
+        With a season that runs from September/October through April/May each
+        year, our Grammy-quality programming embraces bluegrass, jazz,
+        classical, folk, rock, Americana, pop, blues and more, offering
+        something special for everyone who loves great music.To learn more visit
+        <a class="underline" href="https://www.pamlicomusic.org"
+          >www.pamlicomusic.org</a
+        >.
+      </p>
+    </div>
+
+    <div class="p-8 mb-8 bg-pink-100 rounded-lg shadow-lg content-border">
+      <div class="flex justify-between">
+        <div class="w-4/5">
+          <UiHeadline class="mb-2" level="3"
+            >Children's Theater Workshop</UiHeadline
+          >
+          <UiHeadline class="mb-4" level="4"
+            >A Place for Kids to Learn, Grow and Imagine.</UiHeadline
+          >
+        </div>
+
+        <img
+          class="object-contain h-20 ml-1"
+          src="/images/community/ctw-logo.gif"
+          alt="Childrens Theater Workshop logo"
+        />
+      </div>
+      <div class="mb-8">
+        <img
+          src="https://old-theater-pics.netlify.app/img/ctw.png"
+          class="w-full overflow-hidden rounded-lg shadow-lg"
+          alt=""
+        />
+      </div>
+
+      <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
+        Children's Theater Workshop has a longstanding commitment to enriching
+        the lives of children by providing a structured arts program led by an
+        exceptional, and carefully chosen, theater arts Drama Coach.
+      </p>
+
+      <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
+        Besides being great fun, children's theater has many benefits.
+        Activities like improv and drama performance can boost self-esteem and
+        confidence. The theater is a place where inner silliness and creativity
+        are embraced. When children learn to be more confident in themselves, it
+        translates to other aspects of their lives &mdash; school,
+        communication, and social situations.
+      </p>
+      <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
+        During the age-divided, two-week sessions, children are introduced to
+        theater skills such as acting fundamentals, learning to speak on stage,
+        using drama exercises by playing theater games, see the
+        behind-the-scenes work, learn to work in peer groups, and meet new
+        friends.
+      </p>
+      <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
+        Learning about the performing arts can teach important skills in life,
+        such as self-confidence, cooperation, self-discipline, and
+        concentration. Imagination is encouraged! More at
+        <a class="underline" href="https://www.childrenstheateronc.com/"
+          >www.childrenstheateronc.com</a
+        >
+      </p>
+    </div>
+
+    <div
+      class="p-8 pt-4 mb-8 bg-indigo-100 rounded-lg shadow-lg content-border"
+    >
+      <UiHeadline class="mb-4" level="3">Pamlico Community Band</UiHeadline>
+      <div>
+        <img
+          src="https://old-theater-pics.netlify.app/img/community-band.jpg"
+          class="w-full overflow-hidden rounded-lg shadow-lg"
+          alt=""
+        />
+        <p class="p-1 mb-3 text-xs italic text-blue">
+          <!-- Pamlico Community Band -->
+        </p>
+      </div>
+      <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
+        The Pamlico Community Band has members from Pamlico County and
+        surrounding counties. Ages range from the young, to the young at heart!
+        It doesn't matter what experience you have. Only thing that matters, is
+        that you have a passion for music!
+      </p>
+      <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
+        More at their
+        <a
+          class="underline"
+          href="https://www.facebook.com/Pamlico-Community-Band-138279799519612/"
+          >Facebook page</a
+        >
+      </p>
+
+      <div class="flex"></div>
+    </div>
+
+    <div class="p-8 pt-4 mb-8 bg-gray-100 rounded-lg shadow-lg content-border">
+      <UiHeadline class="mb-4" level="3">Pamlico Chorale</UiHeadline>
+
+      <div class="float-right ml-2">
+        <img
+          src="https://old-theater-pics.netlify.app/img/chorale.jpg"
+          class="w-full overflow-hidden rounded-lg"
+          alt=""
+        />
+        <p class="p-1 mb-3 text-xs italic text-blue">
+          <!-- The Pamlico Chorale  -->
+        </p>
+      </div>
+      <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
+        The Pamlico Chorale is an established choral group in Pamlico County,
+        North Carolina. Its members enjoy singing and rejoicing together. Reba
+        Tiller is the group's beloved, long-standing leader. Participation is
+        open to all who share a love of music.
+      </p>
+      <p class="mb-4 leading-7 text-gray-900 text-medium lg:leading-9 lg:mb-6">
+        More at their
+        <a
+          class="underline"
+          href="https://www.facebook.com/Pamlico-Chorale-158261794372262/"
+        >
+          Facebook page</a
+        >
+      </p>
+    </div>
+    <client-only>
+      <member-carousel />
+    </client-only>
   </div>
-</div>
 </template>
 
 <script>
