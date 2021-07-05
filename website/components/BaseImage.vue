a4e55d23331c1cfaf6051970b6637209c7d75d46
Steven Loe <stevenjloe@protonmail.com>
2021-07-05 17:02:29 -0400

    feature: add nuxt-image & nuxt-picture components

---
+++ website/components/BaseImage.vue
@@ -0,0 +1,20 @@
+<template>
+  <nuxt-img v-if="img" class="cover-image" :src="img" :alt="alt" sizes=" xs:100vw sm:100vw md:100vw lg:100vw xl:100vw">
+        </nuxt-img>
+</template>
+
+
+<script>
+export default {
+  props: {
+    alt: {
+      type: String,
+      required: true
+    },
+    img: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
---
+++ website/components/BasePicture.vue
@@ -0,0 +1,36 @@
+<template>
+  <figure class="mb-6">
+    <nuxt-picture
+      v-if="img"
+      class="cover-image"
+      :src="img"
+      :alt="alt"
+      sizes=" xs:100vw sm:100vw md:100vw lg:100vw xl:100vw"
+    >
+    </nuxt-picture>
+    
+    <figcaption v-if="caption" class="italic">
+      {{ caption }}
+    </figcaption>
+  </figure>
+</template>
+
+
+<script>
+export default {
+  props: {
+    alt: {
+      type: String,
+      required: true,
+    },
+    caption: {
+      type: String,
+      required: false,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
