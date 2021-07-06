0397f1803e4a66e30dc4bc4737b295608d8fe3a7
Steven Loe <stevenjloe@protonmail.com>
2021-07-06 11:38:55 -0400

    refactor support slug to use base-picture

--- website/pages/support/_slug.vue
+++ website/pages/support/_slug.vue
@@ -3,6 +3,9 @@
     <div class="w-full max-w-4xl mx-auto lg:text-xl xl:text-2xl">
     <UiHeadline id="level-1-headline" level="1">{{content.title}}</UiHeadline>
       <article class="p-6 mt-6 bg-white rounded shadow-lg lg:p-8 content-border">
+        
+        <base-picture :v-if="content.img" :caption="content.caption" :img="content.img" :alt="content.imgAlt"></base-picture>
+        
         <nuxt-content
           class="mx-auto mb-8 prose-sm prose sm:prose lg:prose-lg xl:prose-2xl"
           :document="content"
@@ -14,6 +17,7 @@
 
 <script>
 import UiHeadline from "@/components/ui/UiHeadline";
+import BasePicture from "@/components/BasePicture";
 import ButtonLink from "@/components/ui/ButtonLink";
 export default {
   async asyncData({ $content, params }) {
@@ -23,7 +27,8 @@ export default {
   layout: "NewLayout",
   components: {
     UiHeadline,
-    ButtonLink
+    ButtonLink,
+    BasePicture
   }
 };
 </script>
