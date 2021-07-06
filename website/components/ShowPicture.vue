29722da3cf32feea3559d5d8f57b3eed1542f16b
Steven Loe <stevenjloe@protonmail.com>
2021-07-06 11:35:03 -0400

    feature: add show-picture: support webp images

---
+++ website/components/ShowPicture.vue
@@ -0,0 +1,55 @@
+<template>
+  <figure class="h-full">
+    <nuxt-picture
+      v-if="img"
+      :fit="fit"
+      :src="img"
+      :alt="alt"
+      sizes=" xs:100vw sm:100vw md:100vw lg:100vw"
+      quality="70"
+      loading="lazy"
+    >
+    </nuxt-picture>
+    
+    <figcaption v-if="caption" class="italic">
+      {{ caption }}
+    </figcaption>
+  </figure>
+</template>
+
+
+<script>
+export default {
+  props: {
+    alt: {
+      type: String,
+      required: true,
+    },
+    caption: {
+      type: String,
+      required: false,
+    },
+    fit: {
+      type: String,
+      required: false,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+  picture {
+  width: 100%;
+  height: 100%;
+  display: flex;
+}
+picture img {
+ object-fit: cover; 
+    height: auto;
+    width:100%;
+}
+</style>
