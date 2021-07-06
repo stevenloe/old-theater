3705ed99f915ba7f740c5dbdc53576b5dbc17b8d
Steven Loe <stevenjloe@protonmail.com>
2021-07-06 16:49:48 -0400

    refactor content slug to use nuxt-image

--- website/pages/about/_slug.vue
+++ website/pages/about/_slug.vue
@@ -2,9 +2,9 @@
    <div>
     <div class="w-full max-w-4xl mx-auto lg:text-xl xl:text-2xl">
     <UiHeadline id="level-1-headline" level="1">{{content.title}}</UiHeadline>
-      <article class="p-6 mt-6 bg-white rounded shadow-lg lg:p-8 content-border">
+      <article class="p-6 mt-6 bg-white rounded-lg shadow-lg lg:p-8 content-border">
         
-        <base-picture :v-if="content.img" :caption="content.caption" :img="content.img" :alt="content.imgAlt"></base-picture>
+        <base-picture class="mb-6" :v-if="content.img" :caption="content.caption" :img="content.img" :alt="content.imgAlt"></base-picture>
         
         <nuxt-content
           class="mx-auto mb-8 prose-sm prose sm:prose lg:prose-lg xl:prose-2xl"
@@ -21,7 +21,6 @@ import BasePicture from "@/components/BasePicture";
 export default {
   async asyncData({ $content, params }) {
     const content = await $content("abouts", params.slug).fetch();
-    console.log("CONTENT", content);
     return { content };
   },
   layout: "NewLayout",
--- website/pages/support/_slug.vue
+++ website/pages/support/_slug.vue
@@ -3,9 +3,7 @@
     <div class="w-full max-w-4xl mx-auto lg:text-xl xl:text-2xl">
     <UiHeadline id="level-1-headline" level="1">{{content.title}}</UiHeadline>
       <article class="p-6 mt-6 bg-white rounded-lg shadow-lg lg:p-8 content-border">
-        
-        
-        
+        <base-picture :v-if="content.img" :caption="content.caption" :img="content.img" :alt="content.imgAlt"></base-picture>
         <nuxt-content
           class="mx-auto mb-8 prose-sm prose sm:prose lg:prose-lg xl:prose-2xl"
           :document="content"
@@ -28,8 +26,6 @@ export default {
   },
   async asyncData({ $content, params }) {
     const content = await $content("supports", params.slug).fetch();
-
-    console.log("CONTENT", content);
     return { content };
   },
   layout: "NewLayout",
