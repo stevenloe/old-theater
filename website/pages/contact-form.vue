28149e081f3fca6907a7d47135e781fa154b8847
Steven Loe <stevenjloe@protonmail.com>
2022-03-22 16:15:51 -0400

    refine contact form

--- website/pages/contact-form.vue
+++ website/pages/contact-form.vue
@@ -8,6 +8,32 @@
         <article
           class="p-6 mt-6 lg:p-8 "
         >
+        <div class="flex flex-wrap">
+            <div class="w-full mb-6 md:w-1/2 md:mb-0">
+              <h2 class="text-2xl font-semibold">Our Physical Address</h2>
+              <address class="mb-8 font-serif not-italic md:text-xl xl:text-xl">
+                The Old Theater<br />
+                609 Broad Street<br />
+                Oriental, NC 28571<br />
+                (252) 249 &ndash; 0477<br />
+              </address>
+            </div>
+            <div class="w-full px-3 mb-6 md:w-1/2">
+              <h2 class="text-2xl font-semibold">Our Mailing Address</h2>
+              <address class="mb-8 font-serif not-italic md:text-xl xl:text-xl">
+                The Old Theater<br />
+                P.O. Box 521<br />
+                Oriental, NC 28571<br />
+              </address>
+            </div>
+          </div>
+          <h2 class="mb-6 text-3xl font-semibold">
+            We’d love to hear from you!
+          </h2>
+          <p class="mb-6">
+            To contact us, please fill out the form below. Or call The Old
+            Theater directly at (252) 249 – 0477.
+          </p>
           <form
             name="contact-form"
             id="contact-form"
@@ -23,15 +49,7 @@
                 />
               </label>
             </p>
-            <h2 class="mb-6 text-3xl font-semibold">We Need You!</h2>
-            <p class="max-w-3xl mb-8 text-xl">
-              As one of our volunteers, you’ll serve as a goodwill ambassador
-              for the Old Theater and will have funworking on outstanding events
-              and productions. No matter what your experience level, the Old
-              Theater has opportunities for you to get involved! Tell us how
-              you’d like to participate in a theater-sponsored event by filling
-              out this form.
-            </p>
+
             <div class="flex flex-wrap mb-6 -mx-3">
               <div class="w-full px-3 mb-6 md:w-1/2 md:mb-0">
                 <label
@@ -63,24 +81,6 @@
                   type="text"
                   name="lastname"
                   placeholder="Doe"
-                  required
-                />
-              </div>
-            </div>
-            <div class="w-full mb-8">
-              <div class="w-full mb-6 md:mb-0">
-                <label
-                  class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase "
-                  for="address"
-                >
-                  address <span class="ml-1 text-gray-600"> (required) </span>
-                </label>
-                <input
-                  class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 rounded appearance-none borderborder-gray-800 focus:bg-white focus:border-gray-900 focus:outline-none"
-                  id="address"
-                  type="text"
-                  name="address"
-                  placeholder="123 Broad Street"
                   required
                 />
               </div>
@@ -118,194 +118,20 @@
                 />
               </div>
             </div>
-            <p class="mb-4 text-2xl font-semibold">
-              Your Interests (check all that apply):
-            </p>
-            <div class="mb-8 text-2xl">
-              <input
-                type="checkbox"
-                value="I'm Interested"
-                class="mr-2 "
-                id="Acting"
-                name="Acting"
-              />
-              <label for="Acting"> Acting </label><br /><input
-                type="checkbox"
-                value="I'm Interested"
-                class="mr-2"
-                id="Production"
-                name="Production"
-              />
-              <label for="Production"> Production </label><br /><input
-                type="checkbox"
-                value="I'm Interested"
-                class="mr-2"
-                id="Director"
-                name="Director"
-              />
-              <label for="Director"> Director </label><br /><input
-                type="checkbox"
-                value="I'm Interested"
-                class="mr-2"
-                id="stage-manager"
-                name="stage-manager"
-              />
-              <label for="stage-manager">
-                Stage Manager/Assistant Stage Manager </label
-              ><br /><input
-                type="checkbox"
-                value="I'm Interested"
-                class="mr-2"
-                id="light-board-operator"
-                name="light-board-operator"
-              />
-              <label for="light-board-operator"> Light Board Operator </label
-              ><br /><input
-                type="checkbox"
-                value="I'm Interested"
-                class="mr-2"
-                id="sound-board-operator"
-                name="sound-board-operator"
-              />
-              <label for="sound-board-operator"> Sound Board Operator </label
-              ><br /><input
-                type="checkbox"
-                value="I'm Interested"
-                class="mr-2"
-                id="set-design"
-                name="set-design"
-              />
-              <label for="set-design"> Set Design </label><br /><input
-                type="checkbox"
-                value="I'm Interested"
-                class="mr-2"
-                id="set-construction"
-                name="set-construction"
-              />
-              <label for="set-construction">
-                Set Construction/Strike, Painting </label
-              ><br /><input
-                type="checkbox"
-                value="I'm Interested"
-                class="mr-2"
-                id="Props"
-                name="Props"
-              />
-              <label for="Props" class="zemCheckbox Props"> Props </label
-              ><br /><input
-                type="checkbox"
-                value="I'm Interested"
-                class="mr-2"
-                id="Wardrobe"
-                name="Wardrobe"
-              />
-              <label for="Wardrobe"> Wardrobe </label><br /><input
-                type="checkbox"
-                value="I'm Interested"
-                class="mr-2"
-                id="Makeup"
-                name="Makeup"
-              />
-              <label for="Makeup"> Makeup </label><br /><input
-                type="checkbox"
-                value="I'm Interested"
-                class="mr-2"
-                id="Stagehand"
-                name="Stagehand"
-              />
-              <label for="Stagehand"> Stagehand </label><br /><input
-                type="checkbox"
-                value="I'm Interested"
-                class="mr-2"
-                id="movie-projection"
-                name="movie-projection"
-              />
-              <label for="movie-projection"> Movie Projection </label
-              ><br /><input
-                type="checkbox"
-                value="I'm Interested"
-                class="mr-2"
-                id="ticket-window"
-                name="ticket-window"
-              />
-              <label for="ticket-window"> Ticket Window </label><br /><input
-                type="checkbox"
-                value="I'm Interested"
-                class="mr-2"
-                id="greeter-concession-usher"
-                name="greeter-concession-usher"
-              />
-              <label for="greeter-concession-usher">
-                Greeter, Concession Sales, Usher </label
-              ><br /><input
-                type="checkbox"
-                value="I'm Interested"
-                class="mr-2"
-                id="admin-support"
-                name="admin-support"
-              />
-              <label for="admin-support"> Administrative Support </label
-              ><br /><input
-                type="checkbox"
-                value="I'm Interested"
-                class="mr-2"
-                id="facility-enhancement"
-                name="facility-enhancement"
-              />
-              <label for="facility-enhancement"> Facility Enhancement </label>
-            </div>
-            <p class="mb-4 text-2xl font-semibold">
-              Availability (check all that apply):
-            </p>
-            <input
-              type="checkbox"
-              value="I'm Available"
-              class="mr-2"
-              id="Weekdays"
-              name="Weekdays"
-            />
-            <label for="Weekdays"> Weekdays </label><br /><input
-              type="checkbox"
-              value="I'm Available"
-              class="mr-2"
-              id="Weekends"
-              name="Weekends"
-            />
-            <label for="Weekends"> Weekends </label><br /><input
-              type="checkbox"
-              value="I'm Available"
-              class="mr-2"
-              id="Mornings"
-              name="Mornings"
-            />
-            <label for="Mornings"> Mornings </label><br /><input
-              type="checkbox"
-              value="I'm Available"
-              class="mr-2"
-              id="Afternoons"
-              name="Afternoons"
-            />
-            <label for="Afternoons"> Afternoons </label><br /><input
-              type="checkbox"
-              value="I'm Available"
-              class="mr-2"
-              id="Evenings"
-              name="Evenings"
-            />
-            <label for="Evenings"> Evenings </label><br />
+       
             <div class="w-full mt-8">
               <label
                 class="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase "
                 for="comments"
               >
-                Comments <span class="ml-1 text-gray-600"> </span> </label
+                Message <span class="ml-1 text-gray-600"> </span> </label
               ><textarea
-                id="comments"
-                name="comments"
+                id="message"
+                name="message"
                 rows="8"
                 cols="50"
                 class="block w-full px-4 py-3 text-gray-700 bg-gray-200 border border-gray-800 rounded appearance-none mb-3leading-tight focus:bg-whitefocus:border-gray-900 focus:outline-none"
-                placeholder="My comments are..."
+                placeholder="My message is..."
                 required
               ></textarea>
               <p class="ml-1 text-xs italic text-gray-600"></p>
