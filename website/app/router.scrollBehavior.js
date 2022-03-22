6618231384ab7f650ea524c973efb576926ed38e
Steven Loe <stevenjloe@protonmail.com>
2022-03-22 11:11:44 -0400

    menu debugging

--- website/app/router.scrollBehavior.js
+++ website/app/router.scrollBehavior.js
@@ -1,4 +1,4 @@
 // on route change, scroll page to top
-export default function (to, from, savedPosition) {
-  return { x: 0, y: 0 }
-}
+// export default function (to, from, savedPosition) {
+//   return { x: 0, y: 0 }
+// }
